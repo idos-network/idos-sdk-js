0935b5d930822353684e7740f4d82a077079e126
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2023-09-27 15:29:51 +0200

    feat(sdk): filter table by given key value pair

--- packages/idos-sdk-js/src/main.js
+++ packages/idos-sdk-js/src/main.js
@@ -5,9 +5,12 @@ import { idOS } from "./lib";
 document.addEventListener("DOMContentLoaded", async () => {
   const web3provider = new ethers.BrowserProvider(window.ethereum);
   await web3provider.send("eth_requestAccounts", []);
-
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const idos = new idOS({ url: "https://nodes.idos.network" });
 
   console.log("idOS SDK initialized");
+
+  await web3provider.send("eth_requestAccounts", []);
+  await idos.auth.setWalletSigner(await web3provider.getSigner());
+  const attributes = await idos.data.list("attributes", { attribute_key: "BAR" });
+  console.log(attributes);
 });
