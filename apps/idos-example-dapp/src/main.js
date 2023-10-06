99aff0a41cbf57b2fa4fb5ef2312926fda01379b
Júlio Santos <hi@whoisjuliosantos.com>
2023-10-06 15:19:22 +0200

    Draft NEAR grants

--- apps/idos-example-dapp/src/main.js
+++ apps/idos-example-dapp/src/main.js
@@ -8,6 +8,8 @@ import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
 import { setupHereWallet } from "@near-wallet-selector/here-wallet";
 import { setupNightly } from "@near-wallet-selector/nightly";
 
+import * as nearAPI from "near-api-js";
+
 import { idOS } from "@idos-network/idos-sdk";
 
 const idos = new idOS({ url: "https://nodes.playground.idos.network", container: "#idOS" });
@@ -19,6 +21,8 @@ const useEvmWallet = async () => {
   const signer = await provider.getSigner();
 
   await idos.auth.setWalletSigner(signer);
+
+  // NOTE if using access grants
   await idos.grants.init({ signer, type: "evm" });
 };
 
@@ -30,20 +34,10 @@ const useNearWallet = async () => {
     modules: [setupMeteorWallet(), setupHereWallet(), setupNightly()],
   });
 
-  // custom selector
-  document.querySelector("#near_wallet_selector").style.display = "block";
-  document.querySelector("#near_wallet_selector form").addEventListener("submit", async (e) => {
-    e.preventDefault();
-    wallet = await selector.wallet(e.submitter.name);
-    document.querySelector("#near_wallet_selector").style.display = "none";
-    walletSelectorReady();
+  const modal = setupModal(selector, {
+    contractId: "idos-dev-1.near",
+    methodNames: ["insert_grant", "find_grants"],
   });
-
-  // We can't use this modal just for wallet selection, because
-  // it will trigger wallet.signIn, which we don't need.
-  // Cons: more signatures for the user
-  // Pros: it's pretty and familiar
-  const modal = setupModal(selector, { contractId: "idos-dev-1.testnet" });
   const subscription = modal.on("onHide", async () => {
     wallet = await selector.wallet();
     walletSelectorReady();
@@ -64,21 +58,41 @@ const useNearWallet = async () => {
     const signature = Uint8Array.from(atob(signMessage.signature), (c) => c.charCodeAt(0));
     return { signature };
   };
+
   idos.auth.setWalletSigner(signer, signMessage.publicKey, "ed25519_nr");
-  await idos.grants.init({ account: signMessage.accountId, wallet: await selector.wallet(), type: "near" });
+
+  // NOTE if using access grants
+  await idos.grants.init({
+    account: signMessage.accountId,
+    wallet: await selector.wallet(),
+    type: "near",
+  });
 };
 
 const useEnclaveSigner = async () => {
   await idos.auth.setEnclaveSigner();
 };
 
+let contract;
+
+
+// NOTE this method can only be used for reading, because keyStore only has public keys
+const findGrants = async ({ owner, grantee, dataId }) => (
+  await contract.find_grants({owner, grantee, data_id: dataId})
+);
+
+
 /*
  * pick one
  */
-await useEvmWallet();
-// await useNearWallet();
-// await useEnclaveSigner();
+//await useEvmWallet();
+await useNearWallet();
+//await useEnclaveSigner();
 
+
+/*
+ * finally
+ */
 const currentUser = await idos.auth.currentUser();
 
 if (!currentUser?.humanId) {
@@ -110,24 +124,3 @@ if (!currentUser?.humanId) {
     });
   });
 }
-
-// console.log(
-//   await idos.grants.create({
-//     grantee: "0x220DB51B3444B0B5CF27319cA2E9486C5E896477",
-//     id: "121",
-//     wait: false,
-//   })
-// );
-
-// console.log(
-//   await idos.grants.list({
-//     grantee: "0x220DB51B3444B0B5CF27319cA2E9486C5E896477",
-//     id: "120",
-//   })
-// );
-
-// console.log(await idos.grants.revoke({
-//   grantee: "0x220DB51B3444B0B5CF27319cA2E9486C5E896477",
-//   id: "121",
-//   wait: false,
-// }));
