fa6b7e61c093829196238d40356bb5121e8a5cc5
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2023-10-06 11:45:24 +0200

    feat: add and remove wallets via the sdk

--- apps/idos-data-dashboard/src/routes/dashboard/wallets/mutations/index.ts
+++ apps/idos-data-dashboard/src/routes/dashboard/wallets/mutations/index.ts
@@ -1,28 +1,15 @@
-import { Utils } from "@kwilteam/kwil-js";
 import { createMutation } from "react-query-kit";
 
-import { kwil } from "@/lib/db";
-import { buildTx } from "@/lib/mutations";
+import { idos } from "@/lib/idos";
 
 export const useCreateWallet = createMutation({
   mutationFn: async ({ address }: { address: string }) => {
-    const inputs = new Utils.ActionInput()
-      .put("$id", crypto.randomUUID())
-      .put("$address", address)
-      .put("$signature", "N/A")
-      .put("$message", "N/A")
-      .put("$issuer", "N/A");
-    const tx = await buildTx("add_wallet", inputs);
-
-    return kwil.broadcast(tx);
+    return idos.data.create("wallets", { address, signature: "", message: "" });
   },
 });
 
 export const useRemoveWallet = createMutation({
   mutationFn: async ({ id }: { id: string }) => {
-    const inputs = new Utils.ActionInput().put("$id", id);
-    const tx = await buildTx("remove_wallet", inputs);
-
-    return kwil.broadcast(tx);
+    return idos.data.delete("wallets", id);
   },
 });
--- apps/idos-data-dashboard/src/routes/dashboard/wallets/queries/index.ts
+++ apps/idos-data-dashboard/src/routes/dashboard/wallets/queries/index.ts
@@ -1,15 +1,11 @@
 import { createQuery } from "react-query-kit";
 
-import { kwil } from "@/lib/db";
-import { buildMsg } from "@/lib/queries";
-import { castToType } from "@/lib/types";
-
+import { idos } from "@/lib/idos";
 import { Wallet } from "../types";
 
 export const useFetchWallets = createQuery({
   primaryKey: "wallets",
   queryFn: async () => {
-    const tx = await buildMsg("get_wallets");
-    return kwil.call(tx).then((res) => castToType<Wallet[]>(res.data?.result || []));
+    return idos.data.list<Wallet>("wallets");
   },
 });
