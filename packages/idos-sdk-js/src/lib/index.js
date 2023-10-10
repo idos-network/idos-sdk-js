1f74a4471b95c98736f085ff2095bad1d4a2d03f
Júlio Santos <hi@whoisjuliosantos.com>
2023-10-10 18:17:35 +0200

    Tweak and update interface after recent changes. Use id for records and dataId for grants

--- packages/idos-sdk-js/src/lib/index.js
+++ packages/idos-sdk-js/src/lib/index.js
@@ -3,14 +3,17 @@ import { Crypto } from "./crypto";
 import { Data } from "./data";
 import { Grants } from "./grants/grants";
 import { KwilWrapper } from "./kwil-wrapper";
+import { Utils } from "./utils";
 
 export class idOS {
   constructor(options) {
     this.kwilWrapper = new KwilWrapper({ url: options.url });
     this.container = options.container;
-    this.auth = new Auth(this);
+
     this.crypto = new Crypto(this);
+    this.auth = new Auth(this);
     this.data = new Data(this);
     this.grants = new Grants(this);
+    this.utils = Utils;
   }
 }
