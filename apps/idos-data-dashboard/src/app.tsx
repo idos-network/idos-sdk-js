741ff200b71a50e07270fa55d11c2bddadc9061f
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2023-10-10 18:26:40 +0200

    feat: wip connect wallet

--- apps/idos-data-dashboard/src/app.tsx
+++ apps/idos-data-dashboard/src/app.tsx
@@ -1,25 +1,29 @@
 import { Box } from "@chakra-ui/react";
-import { BrowserProvider } from "ethers";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { useEffectOnce } from "usehooks-ts";
 
 import { Header } from "@/lib/components/header";
 import { Loading } from "@/lib/components/loading";
-import { idos } from "@/lib/idos";
+import { ConnectWallet } from "./lib/components/connect-wallet";
 
 export default function App() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isConnected, setIsConnected] = useState(false);
 
   useEffectOnce(() => {
     async function setup() {
-      await idos.crypto.init();
-      const web3provider = new BrowserProvider(window.ethereum);
-      await web3provider.send("eth_requestAccounts", []);
-      await idos.auth.setWalletSigner(await web3provider.getSigner());
+      // await idos.crypto.init();
+      // const web3provider = new BrowserProvider(window.ethereum);
+      // const accounts = await web3provider.send("eth_requestAccounts", []);
+      // await idos.auth.setWalletSigner(await web3provider.getSigner());
     }
-    setup().then(() => setIsLoading(false));
+    setup();
   });
+
+  if (!isConnected) {
+    return <ConnectWallet />;
+  }
 
   if (isLoading) {
     return (
