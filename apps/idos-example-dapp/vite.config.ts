d5b8480e0eea07473d99c94725e79f5c341677af
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2023-10-20 14:14:16 +0200

    chore: update build target for example dapp

--- apps/idos-example-dapp/vite.config.ts
+++ apps/idos-example-dapp/vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from "vite"
-import { nodePolyfills } from "vite-plugin-node-polyfills"
+import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
   plugins: [
     nodePolyfills({
       include: ["buffer"],
       globals: {
         Buffer: true,
-      }
+      },
     }),
   ],
-})
+});
