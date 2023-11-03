9bb4a274236dd0532ec21ff4955f64e61f999518
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2023-11-03 16:23:32 +0200

    feat: add wallet address icons

--- apps/idos-data-dashboard/src/routes/dashboard/wallets/components/wallet-card.tsx
+++ apps/idos-data-dashboard/src/routes/dashboard/wallets/components/wallet-card.tsx
@@ -1,12 +1,18 @@
-import { Button, HStack, Text, VStack } from "@chakra-ui/react";
+import Metamask from "#/assets/metamask.svg";
+import Near from "#/assets/near.svg";
+import { Button, Center, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import { XIcon } from "lucide-react";
 import { Wallet } from "../queries";
-
 type WalletCardProps = {
   wallet: Wallet;
   onDeleteWallet: (wallet: Wallet) => void;
 };
 export const WalletCard = (props: WalletCardProps) => {
+  const { wallet } = props;
+  const isNear = wallet.address.includes("near");
+  const imageSrc = isNear ? Near : Metamask;
+  const imageAlt = isNear ? "Near" : "Metamask";
+
   return (
     <HStack
       alignItems="center"
@@ -19,14 +25,19 @@ export const WalletCard = (props: WalletCardProps) => {
       borderColor="neutral.800"
       rounded="xl"
     >
-      <VStack alignItems="start">
-        <Text color="neutral.600" fontSize="sm">
-          Address
-        </Text>
-        <Text w={240} isTruncated>
-          {props.wallet.address}
-        </Text>
-      </VStack>
+      <Center gap={8}>
+        <Center w={12} h={12} bg="neutral.800" rounded="lg">
+          <Image alt={imageAlt} src={imageSrc} />
+        </Center>
+        <VStack alignItems="start" gap={0}>
+          <Text color="neutral.600" fontSize="sm">
+            Address
+          </Text>
+          <Text w={240} isTruncated>
+            {props.wallet.address}
+          </Text>
+        </VStack>
+      </Center>
       <Button
         leftIcon={<XIcon size={24} />}
         onClick={() => props.onDeleteWallet(props.wallet)}
