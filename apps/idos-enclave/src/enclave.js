a8b02c0a8fb1cd1875bac2561852610c559050a8
Júlio Santos <hi@whoisjuliosantos.com>
2023-11-10 09:13:38 +0100

    Fix error inception

--- apps/idos-enclave/src/enclave.js
+++ apps/idos-enclave/src/enclave.js
@@ -125,7 +125,7 @@ export class Enclave {
       throw Error(`Couldn't encrypt. ${JSON.stringify({
         message: Base64Codec.encode(message),
         nonce: Base64Codec.encode(nonce),
-        senderPublicKey: Base64Codec.encode(senderPublicKey),
+        receiverPublicKey: Base64Codec.encode(receiverPublicKey),
         localPublicKey: Base64Codec.encode(this.keyPair.publicKey),
       }, null, 2)}`);
 
