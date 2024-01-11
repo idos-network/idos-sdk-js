163dbdec91cdec12da8c8feb60ae8af6141b9251
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2024-01-11 19:16:54 +0200

    feat(dashboard): use toasts for wallet mutation error messages

--- apps/idos-data-dashboard/src/theme.ts
+++ apps/idos-data-dashboard/src/theme.ts
@@ -1,6 +1,6 @@
 import { extendBaseTheme, theme as chakraTheme } from "@chakra-ui/react";
 
-const { Button, CloseButton, Drawer, FormLabel, Heading, Input, Modal, Spinner } =
+const { Alert, Button, CloseButton, Drawer, FormLabel, Heading, Input, Modal, Spinner, } =
   chakraTheme.components;
 
 export const theme = extendBaseTheme({
@@ -42,6 +42,7 @@ export const theme = extendBaseTheme({
   },
 
   components: {
+    Alert,
     Button,
     CloseButton,
     Drawer,
