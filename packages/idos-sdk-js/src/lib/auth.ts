a29a437e0f1a5045e187cdd43694de9f7413b1f2
Alexandr Kovalenko <alex.sapience@gmail.com>
2024-03-12 12:10:08 +0300

    Get and use implicit account for Near

--- packages/idos-sdk-js/src/lib/auth.ts
+++ packages/idos-sdk-js/src/lib/auth.ts
@@ -6,9 +6,12 @@ import * as BytesCodec from "@stablelib/bytes";
 import * as Utf8Codec from "@stablelib/utf8";
 import * as BorshCodec from "borsh";
 import type { Signer } from "ethers";
-import { SigningKey, hashMessage } from "ethers";
+import { SigningKey, hashMessage, decodeBase58, toBeHex } from "ethers";
+import * as nearAPI from "near-api-js";
+
 import { idOS } from "./idos";
 import { Nonce } from "./nonce";
+
 
 /* global Buffer */
 
@@ -174,8 +177,24 @@ export class Auth {
       );
     };
 
+    const { connect } = nearAPI;
+    const connectionConfig = {
+      networkId: import.meta.env.VITE_IDOS_NEAR_DEFAULT_NETWORK,
+      nodeUrl: import.meta.env.VITE_IDOS_NEAR_DEFAULT_RPC_URL,
+    };
+    const nearConnection = await connect(connectionConfig);
+
+    const response = await nearConnection.connection.provider.query({
+      request_type: "view_access_key_list",
+      finality: "final",
+      account_id: currentAddress,
+    });
+    const public_key_with_prefix = response.keys.find((element: object) => element.access_key.permission == "FullAccess").public_key;
+    const public_key = public_key_with_prefix.replace(/^ed25519:/, "");
+    const implicitAccount = toBeHex(decodeBase58(public_key));
+
     return this.#setSigner({
-      accountId: currentAddress,
+      accountId: implicitAccount,
       signer,
       publicKey,
       signatureType: "nep413"
