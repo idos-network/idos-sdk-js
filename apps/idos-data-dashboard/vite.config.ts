ab80b9a4628b2d7453660ebb4c7e0b04b10b07d6
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2024-04-09 22:27:16 +0300

    chore: remove unneeded package

--- apps/idos-data-dashboard/vite.config.ts
+++ apps/idos-data-dashboard/vite.config.ts
@@ -2,7 +2,6 @@ import { resolve } from "path";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-// import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -33,7 +32,6 @@ export default defineConfig({
         Buffer: true
       }
     })
-    // mkcert()
   ],
   resolve: {
     dedupe: ["ethers", "near-api-js", "@near-wallet-selector/core"],
