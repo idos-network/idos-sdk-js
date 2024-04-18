a1a461a5876824af37ada46b4dc7fedc08d47b62
Paulo Koch <pkoch@fractal.id>
2024-04-18 16:16:04 +0100

    feat: add idOSGrantee API

---
+++ packages/idos-sdk-js/src/lib/idos-grantee.ts
@@ -0,0 +1,246 @@
+import crypto from "node:crypto";
+import { KwilSigner, NodeKwil } from "@kwilteam/kwil-js";
+import * as Base64Codec from "@stablelib/base64";
+import * as BinaryCodec from "@stablelib/binary";
+import * as BytesCodec from "@stablelib/bytes";
+import * as sha256 from "@stablelib/sha256";
+import * as Utf8Codec from "@stablelib/utf8";
+import * as BorshCodec from "borsh";
+import { ethers } from "ethers";
+import * as nearAPI from "near-api-js";
+import nacl from "tweetnacl";
+import { assertNever } from "../types";
+import { KwilWrapper } from "./kwil-wrapper";
+import { implicitAddressFromPublicKey } from "./utils";
+
+export type WalletType = "EVM" | "NEAR";
+
+const kwilNep413Signer =
+  (recipient: string) =>
+  (keyPair: nearAPI.KeyPair) =>
+  async (messageBytes: Uint8Array): Promise<Uint8Array> => {
+    const message = Utf8Codec.decode(messageBytes);
+    const nonceLength = 32;
+    const nonce = crypto.getRandomValues(new Uint8Array(nonceLength));
+
+    const nep413BorschSchema = {
+      struct: {
+        message: "string",
+        nonce: { array: { type: "u8", len: nonceLength } },
+        recipient: "string",
+        callbackUrl: { option: "string" }
+      }
+    } as const;
+
+    const tag = 2147484061; // 2**31 + 413
+
+    const { signature } = await keyPair.sign(
+      sha256.hash(
+        BytesCodec.concat(
+          BorshCodec.serialize("u32", tag),
+          BorshCodec.serialize(nep413BorschSchema, { message, nonce, recipient })
+        )
+      )
+    );
+
+    const kwilNep413BorschSchema = {
+      struct: {
+        tag: "u32",
+        ...nep413BorschSchema.struct
+      }
+    };
+
+    const kwilNep413BorshParams = {
+      tag,
+      message,
+      nonce,
+      recipient
+    };
+
+    const kwilNep413BorshPayload = BorshCodec.serialize(
+      kwilNep413BorschSchema,
+      kwilNep413BorshParams
+    );
+
+    return BytesCodec.concat(
+      BinaryCodec.writeUint16BE(kwilNep413BorshPayload.length),
+      kwilNep413BorshPayload,
+      signature
+    );
+  };
+
+export class NoncedBox {
+  keyPair: nacl.BoxKeyPair;
+
+  constructor(keyPair: nacl.BoxKeyPair) {
+    this.keyPair = keyPair;
+  }
+
+  static fromBase64SecretKey(secret: string): NoncedBox {
+    return new NoncedBox(nacl.box.keyPair.fromSecretKey(Base64Codec.decode(secret)));
+  }
+
+  async decrypt(b64FullMessage: string, b64SenderPublicKey: string): Promise<string> {
+    const fullMessage = Base64Codec.decode(b64FullMessage);
+    const senderPublicKey = Base64Codec.decode(b64SenderPublicKey);
+
+    const nonce = fullMessage.slice(0, nacl.box.nonceLength);
+    const message = fullMessage.slice(nacl.box.nonceLength, fullMessage.length);
+
+    const decrypted = nacl.box.open(message, nonce, senderPublicKey, this.keyPair.secretKey);
+
+    if (decrypted == null) {
+      throw Error(
+        `Couldn't decrypt. ${JSON.stringify(
+          {
+            fullMessage: Base64Codec.encode(fullMessage),
+            message: Base64Codec.encode(message),
+            nonce: Base64Codec.encode(nonce),
+            senderPublicKey: Base64Codec.encode(senderPublicKey),
+            receiverPublicKey: Base64Codec.encode(this.keyPair.publicKey)
+          },
+          null,
+          2
+        )}`
+      );
+    }
+
+    return Utf8Codec.decode(decrypted);
+  }
+}
+
+type ChainType = "EVM" | "NEAR";
+
+const buildKwilSignerAndGrantee = (
+  chainType: ChainType,
+  granteeSigner: nearAPI.utils.key_pair.KeyPair | ethers.Wallet
+): [KwilSigner, string] => {
+  switch (chainType) {
+    case "EVM": {
+      const signer = granteeSigner as ethers.Wallet;
+      return [new KwilSigner(signer, signer.address), signer.address];
+    }
+    case "NEAR": {
+      const signer = granteeSigner as nearAPI.utils.key_pair.KeyPair;
+      const publicKey = signer.getPublicKey().toString();
+      return [
+        new KwilSigner(
+          kwilNep413Signer("idos-grantee")(signer),
+          implicitAddressFromPublicKey(publicKey),
+          "nep413"
+        ),
+        publicKey
+      ];
+    }
+    default:
+      return assertNever(chainType, `Unexpected chainType: ${chainType}`);
+  }
+};
+
+interface idOSGranteeInitParams {
+  encryptionSecret: string;
+  nodeUrl?: string;
+  chainId?: string;
+  dbId?: string;
+  chainType: ChainType;
+  granteeSigner: nearAPI.utils.key_pair.KeyPair | ethers.Wallet;
+}
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+export class idOSGrantee {
+  noncedBox: NoncedBox;
+  nodeKwil: NodeKwil;
+  kwilSigner: KwilSigner;
+  dbId: string;
+  chainType: ChainType;
+  address: string;
+
+  static async init({
+    encryptionSecret,
+    nodeUrl = KwilWrapper.defaults.kwilProvider,
+    chainId,
+    dbId,
+    chainType,
+    granteeSigner
+  }: idOSGranteeInitParams) {
+    const kwil = new NodeKwil({ kwilProvider: nodeUrl, chainId: "" });
+
+    chainId ||=
+      // biome-ignore lint/style/noNonNullAssertion: I wanna let it fall to throwError.
+      (await kwil.chainInfo()).data?.chain_id! ||
+      throwError("Can't discover chainId. You must pass it explicitly.");
+
+    dbId ||=
+      // biome-ignore lint/style/noNonNullAssertion: I wanna let it fall to throwError.
+      (await kwil.listDatabases()).data?.filter(({ name }) => name === "idos")[0].dbid! ||
+      throwError("Can't discover dbId. You must pass it explicitly.");
+
+    const nodeKwil = new NodeKwil({ kwilProvider: nodeUrl, chainId });
+
+    const [kwilSigner, address] = buildKwilSignerAndGrantee(chainType, granteeSigner);
+
+    return new idOSGrantee(
+      NoncedBox.fromBase64SecretKey(encryptionSecret),
+      nodeKwil,
+      kwilSigner,
+      dbId,
+      chainType,
+      address
+    );
+  }
+
+  private constructor(
+    noncedBox: NoncedBox,
+    nodeKwil: NodeKwil,
+    kwilSigner: KwilSigner,
+    dbId: string,
+    chainType: ChainType,
+    address: string
+  ) {
+    this.noncedBox = noncedBox;
+    this.nodeKwil = nodeKwil;
+    this.kwilSigner = kwilSigner;
+    this.dbId = dbId;
+    this.chainType = chainType;
+    this.address = address;
+  }
+
+  async fetchSharedCredentialFromIdos<T extends Record<string, unknown>>(
+    dataId: string
+  ): Promise<T> {
+    return (
+      (await this.nodeKwil.call(
+        {
+          action: "get_credential_shared",
+          dbid: this.dbId,
+          inputs: [{ $id: dataId }]
+        },
+        this.kwilSigner
+        // biome-ignore lint/suspicious/noExplicitAny: NodeKwil doesn't have the best type defs.
+      )) as any
+    ).data.result[0] as unknown as T;
+  }
+
+  async getSharedCredentialContentDecrypted(dataId: string): Promise<string> {
+    const credentialCopy = await this.fetchSharedCredentialFromIdos<{
+      content: string;
+      encryption_public_key: string;
+    }>(dataId);
+
+    return await this.noncedBox.decrypt(
+      credentialCopy.content,
+      credentialCopy.encryption_public_key
+    );
+  }
+
+  get grantee() {
+    return this.address;
+  }
+
+  get encryptionPublicKey() {
+    return Base64Codec.encode(this.noncedBox.keyPair.publicKey);
+  }
+}
