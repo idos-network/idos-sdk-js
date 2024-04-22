c4be073df190a425e3096e702a8522b43f13d12b
Paulo Koch <pkoch@fractal.id>
2024-04-22 16:48:23 +0100

    chore: make type signatures a bit better

--- apps/idos-data-dashboard/src/routes/dashboard/wallets/components/add-wallet.tsx
+++ apps/idos-data-dashboard/src/routes/dashboard/wallets/components/add-wallet.tsx
@@ -99,9 +99,9 @@ export const AddWallet = ({ isOpen, onClose }: AddWalletProps) => {
     let publicKeys: string[] = [];
 
     if (address_type === "NEAR") {
-      publicKeys = await getNearFullAccessPublicKeys(address);
+      publicKeys = (await getNearFullAccessPublicKeys(address)) || [];
 
-      if (!publicKeys?.length) {
+      if (!publicKeys.length) {
         toast({
           title: "Error while adding wallet",
           description:
