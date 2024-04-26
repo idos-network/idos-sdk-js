6396e32059d96805cf39be55ef88d16d3ddfc095
Paulo Koch <pkoch@fractal.id>
2024-04-26 21:45:19 +0100

    chore: make store only use local storage (#188)

--- packages/idos-store/index.ts
+++ packages/idos-store/index.ts
@@ -5,14 +5,6 @@ interface PipeCodecArgs<T> {
 
 export class Store {
   keyPrefix = "idOS-";
-  cookieExpiries = {
-    past: new Date(0).toUTCString(),
-    future: new Date(Date.now() + 365 * 3600 * 24 * 1000).toUTCString(),
-  };
-
-  constructor() {
-    this.#rebuild();
-  }
 
   pipeCodec<T>({ encode, decode }: PipeCodecArgs<T>) {
     return {
@@ -27,111 +19,33 @@ export class Store {
   }
 
   get(key: string): any {
-    const values = [
-      this.#getCookie(key),
-      this.#getLocalStorage(key),
-      this.#getSessionStorage(key),
-    ].filter(Boolean) as string[];
-
-    if (!values.length) return;
-
-    if (!values.every((value) => value === values[0]))
-      console.warn(
-        [
-          `Inconsistent idOS store data (${window.location.origin})`,
-          `Key: ${key}`,
-          `${values.join("\nvs\n")}`,
-        ].join("\n"),
-      );
-
-    return values[0] ? JSON.parse(values[0]) : undefined;
+    const value = this.#getLocalStorage(key);
+    if (!value) return undefined;
+    return JSON.parse(value);
   }
 
   set(key: string, value: any, days?: number | string) {
     if (!key || typeof key !== "string") throw new Error(`Bad key: ${key}`);
     if (!value) return;
 
-    const daysNumber = !days || Number.isNaN(Number(days)) ? undefined : parseInt(days.toString());
+    //@ts-ignore TODO Actually use this.
+    const _daysNumber = !days || Number.isNaN(Number(days)) ? undefined : parseInt(days.toString());
 
-    value = JSON.stringify(value);
-
-    if (daysNumber === 0) {
-      this.#setSessionStorage(key, value);
-    } else {
-      this.#setCookie(key, value, daysNumber);
-      this.#setLocalStorage(key, value);
-    }
-  }
-
-  #rebuild() {
-    const keysInCookies: string[] = Object.values(
-      Object.fromEntries(document.cookie.matchAll(new RegExp(`(${"idOS-"}.*?)=`, "g"))),
-    );
-
-    const keysInLocalStorage = Object.keys(window.localStorage);
-
-    for (let key of [...new Set(keysInCookies.concat(keysInLocalStorage))]) {
-      if (!key) continue;
-      key = key.replace(this.keyPrefix, "");
-      this.set(key, this.get(key));
-    }
-  }
-
-  #getCookie(key: string) {
-    return document.cookie.match(`${this.keyPrefix}${key}=(.*?)(;|$)`)?.at(1);
-  }
-
-  #setCookie(key: string, value: any, days?: number) {
-    const expiry = days
-      ? new Date(Date.now() + days * 86400 * 1000).toUTCString()
-      : this.cookieExpiries.future;
-
-    document.cookie = [
-      `${this.keyPrefix}${key}=${value}`,
-      "SameSite=None",
-      "Secure",
-      `Expires=${expiry}`,
-    ].join(";");
+    this.#setLocalStorage(key, JSON.stringify(value));
   }
 
   #getLocalStorage(key: string) {
     return window.localStorage.getItem(`${this.keyPrefix}${key}`);
   }
 
-  #getSessionStorage(key: string) {
-    return window.sessionStorage.getItem(`${this.keyPrefix}${key}`);
-  }
-
   #setLocalStorage(key: string, value: string) {
     return window.localStorage.setItem(`${this.keyPrefix}${key}`, value);
   }
 
-  #setSessionStorage(key: string, value: string) {
-    return window.sessionStorage.setItem(`${this.keyPrefix}${key}`, value);
-  }
-
   reset() {
-    for (const storage of [window.localStorage, window.sessionStorage]) {
-      for (const key of Object.keys(storage)) {
-        if (key === "idOS-credential-id") continue;
-        key.startsWith(this.keyPrefix) && storage.removeItem(key);
-      }
-    }
-
-    const keysInCookies: string[] = Object.values(
-      Object.fromEntries(document.cookie.matchAll(new RegExp(`(${"idOS-"}.*?)=`, "g"))),
-    );
-
-    for (const key of keysInCookies) {
+    for (const key of Object.keys(window.localStorage)) {
       if (key === "idOS-credential-id") continue;
-      key.startsWith(this.keyPrefix) &&
-        (document.cookie = [
-          `${key}=`,
-          "SameSite=None",
-          "Secure",
-          "Path=/",
-          `Expires=${this.cookieExpiries.past}`,
-        ].join(";"));
+      if (key.startsWith(this.keyPrefix)) window.localStorage.removeItem(key);
     }
   }
 }
