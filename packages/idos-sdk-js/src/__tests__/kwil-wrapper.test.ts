599dbbebdc9d61a4ee90b2b5f032cb8ecad90bed
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2024-05-07 16:11:22 +0300

    chore: formatting

--- packages/idos-sdk-js/src/__tests__/kwil-wrapper.test.ts
+++ packages/idos-sdk-js/src/__tests__/kwil-wrapper.test.ts
@@ -27,57 +27,53 @@ describe("kwil-wrapper", () => {
     expect(kwilWrapper.dbId).toBe(dbId);
   });
 
-  test('should set a `secp256k1_ep` signer', async () => {
-    const signer: any = Wallet.createRandom()
+  test("should set a `secp256k1_ep` signer", async () => {
+    const signer: any = Wallet.createRandom();
 
     await kwilWrapper.setSigner({
       accountId: signer.address,
       signer,
-      signatureType: 'secp256k1_ep'
-    })
+      signatureType: "secp256k1_ep",
+    });
 
-    expect(kwilWrapper.signer?.signatureType).toBe('secp256k1_ep')
-  })
+    expect(kwilWrapper.signer?.signatureType).toBe("secp256k1_ep");
+  });
 
-  test('should set a `nep413` signer', async () => {
-    const signer = () => Promise.resolve(new Uint8Array())
-    const pk = 'ed25519:ACWH3UoS3JVnVhmvHMghKN9K76HqiYQpwodJbJCUtciY'
-    const accountId = implicitAddressFromPublicKey(pk)
+  test("should set a `nep413` signer", async () => {
+    const signer = () => Promise.resolve(new Uint8Array());
+    const pk = "ed25519:ACWH3UoS3JVnVhmvHMghKN9K76HqiYQpwodJbJCUtciY";
+    const accountId = implicitAddressFromPublicKey(pk);
     await kwilWrapper.setSigner({
       accountId,
       signer,
-      signatureType: 'nep413'
-    })
+      signatureType: "nep413",
+    });
 
-    expect(kwilWrapper.signer?.signatureType).toBe('nep413')
-  })
+    expect(kwilWrapper.signer?.signatureType).toBe("nep413");
+  });
 
-
-  test('should build a proper action payload with empty inputs and no description', async () => {
-    const payload = await kwilWrapper.buildAction('do something', null)
-
-    expect(payload).toEqual({
-      action: 'do something',
-      dbid: kwilWrapper.dbId,
-      inputs: []
-    })
-  })
-
-  test('should build a proper action payload with all values passed', async () => {
-    const inputs = { key_1: 'value_1', key_2: 'value_2' }
-    const payload = await kwilWrapper.buildAction('do something', [inputs], 'some description')
-    const actionInput = new Utils.ActionInput()
-    actionInput.put('$key_1', 'value_1')
-    actionInput.put('$key_2', 'value_2')
+  test("should build a proper action payload with empty inputs and no description", async () => {
+    const payload = await kwilWrapper.buildAction("do something", null);
 
     expect(payload).toEqual({
-      action: 'do something',
+      action: "do something",
       dbid: kwilWrapper.dbId,
-      description: '*some description*',
-      inputs: [
-        actionInput
-      ]
-    })
-  })
+      inputs: [],
+    });
+  });
 
+  test("should build a proper action payload with all values passed", async () => {
+    const inputs = { key_1: "value_1", key_2: "value_2" };
+    const payload = await kwilWrapper.buildAction("do something", [inputs], "some description");
+    const actionInput = new Utils.ActionInput();
+    actionInput.put("$key_1", "value_1");
+    actionInput.put("$key_2", "value_2");
+
+    expect(payload).toEqual({
+      action: "do something",
+      dbid: kwilWrapper.dbId,
+      description: "*some description*",
+      inputs: [actionInput],
+    });
+  });
 });
