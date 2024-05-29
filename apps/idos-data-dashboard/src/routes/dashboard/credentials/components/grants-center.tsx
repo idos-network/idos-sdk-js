800f6b8525f974ebf6dca05afd17ed6ad19dd28f
ditoglez <ditoglez@gmail.com>
2024-05-29 11:19:14 +0300

    feat: add selector ids for e2e testing (#217)

--- apps/idos-data-dashboard/src/routes/dashboard/credentials/components/grants-center.tsx
+++ apps/idos-data-dashboard/src/routes/dashboard/credentials/components/grants-center.tsx
@@ -33,13 +33,18 @@ type GrantsCenterProps = {
   onClose: () => void;
 };
 
+function generateGrantId(grant: idOSGrant): string {
+  const { dataId, grantee, owner, lockedUntil } = grant;
+  return [dataId, grantee, owner, lockedUntil].join("-");
+}
+
 function bigintToDate(bi: bigint): string {
   const milliseconds = Number(bi) * 1000;
 
   return new Intl.DateTimeFormat(["ban", "id"]).format(new Date(milliseconds));
 }
 
-const Shares = ({ grants }: { credentialId: string; grants: idOSGrant[] }) => {
+const Shares = ({ credentialId, grants }: { credentialId: string; grants: idOSGrant[] }) => {
   const revokeGrant = useRevokeGrant();
   const queryClient = useQueryClient();
 
@@ -67,7 +72,7 @@ const Shares = ({ grants }: { credentialId: string; grants: idOSGrant[] }) => {
         </Text>
       </Stack>
       <TableContainer rounded="lg" bg="neutral.800" border="1px solid" borderColor="neutral.700">
-        <Table variant="simple" w="100%">
+        <Table id={`grants-for-${credentialId}`} variant="simple" w="100%">
           <Thead>
             <Tr>
               <Th color="neutral.500">Grantee</Th>
@@ -77,7 +82,11 @@ const Shares = ({ grants }: { credentialId: string; grants: idOSGrant[] }) => {
           </Thead>
           <Tbody>
             {grants.map((grant) => (
-              <Tr key={grant.dataId}>
+              <Tr
+                key={generateGrantId(grant)}
+                id={`grant-${generateGrantId(grant)}`}
+                data-grant={JSON.stringify(grant)}
+              >
                 <Td maxW={140}>
                   <Text isTruncated>{grant.grantee}</Text>
                 </Td>
@@ -86,12 +95,13 @@ const Shares = ({ grants }: { credentialId: string; grants: idOSGrant[] }) => {
                 </Td>
                 <Td isNumeric>
                   <Button
+                    id={`revoke-grant-${generateGrantId(grant)}`}
                     size="sm"
                     variant="outline"
                     colorScheme="red"
                     isDisabled={Boolean(
                       grant.lockedUntil &&
-                        BigInt(grant.lockedUntil) <= BigInt(Math.floor(Date.now() / 1000)),
+                        BigInt(grant.lockedUntil) < BigInt(Math.floor(Date.now() / 1000)),
                     )}
                     isLoading={
                       revokeGrant.isPending && revokeGrant.variables?.dataId === grant.dataId
@@ -142,14 +152,8 @@ export const GrantsCenter = ({ credentialId, isOpen, onClose }: GrantsCenterProp
             <Center flex={1}>
               <Spinner />
             </Center>
-          ) : (
-            false
-          )}
-          {grants.isError ? (
-            <Text color="red.500">Something went wrong, please retry.</Text>
-          ) : (
-            false
-          )}
+          ) : null}
+          {grants.isError ? <Text color="red.500">Something went wrong, please retry.</Text> : null}
           {grants.isSuccess ? <Shares credentialId={credentialId} grants={grants.data} /> : false}
           <Code />
         </ModalBody>
