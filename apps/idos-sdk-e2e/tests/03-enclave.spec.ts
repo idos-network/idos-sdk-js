004d6f06de526ad7448489d0590646858b97b867
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2024-05-31 21:57:26 +0300

    chore: update e2e credential content

--- apps/idos-sdk-e2e/tests/03-enclave.spec.ts
+++ apps/idos-sdk-e2e/tests/03-enclave.spec.ts
@@ -8,19 +8,19 @@ const credentialContent = `{
     "https://raw.githubusercontent.com/trustfractal/claim-schemas/686bd9c0b44f8af03831f7dc31f7d6a9b6b5ff5b/verifiable_credential/fractal_id.json-ld",
     "https://w3id.org/security/suites/ed25519-2020/v1"
   ],
-  "id": "uuid:8b38a9ad-58c9-4758-84f0-0f99e04b658a",
+  "id": "uuid:201149e1-668a-413f-9931-29b2a6828f83",
   "type": [
     "VerifiableCredential"
   ],
   "issuer": "https://vc-issuers.next.fractal.id/idos",
   "level": "human",
   "credentialSubject": {
-    "id": "uuid:f1839b5c-af8e-473e-9980-fb7713a48561",
+    "id": "uuid:0b764d0c-457e-483b-9dab-79cd034f3ac5",
     "wallets": [
       {
         "currency": "eth",
         "verified": true,
-        "address": "0x5f6015c69059ca003354a2917827ce718cf01c0c"
+        "address": "0xdbf901f529a85f0fc309ee4d3ede01c6e648cf54"
       }
     ],
     "emails": [
@@ -31,17 +31,17 @@ const credentialContent = `{
     ]
   },
   "status": "approved",
-  "issuanceDate": "2024-05-24T15:25:51Z",
-  "approved_at": "2024-05-24T15:25:51Z",
+  "issuanceDate": "2024-05-31T18:47:33Z",
+  "approved_at": "2024-05-31T18:47:33Z",
   "proof": {
     "type": "Ed25519Signature2020",
-    "created": "2024-05-24T15:26:13Z",
+    "created": "2024-05-31T18:47:37Z",
     "verificationMethod": "https://vc-issuers.next.fractal.id/idos#z6Mkqm5JuLvBcHkbQogDXz5p5ppTY4DF5FLhoRd2VM9NaKp5",
     "proofPurpose": "assertionMethod",
     "@context": [
       "https://www.w3.org/ns/credentials/v2"
     ],
-    "proofValue": "z2VqZk9KVf1LgGQ15Rk3genW6EhN5aKnZnutXPc4oPurenC9j1bUVKBLUgTCu4dmLixLAXgChUWmBeiXrZ8hzXj3"
+    "proofValue": "z3UD9q9Y8TSxC8fY7RSj1YRhAkScgQgHVwn6EKguLEX69GxV3rz9JNJwik3v3W8LtWTnU8gZYBV6EgpqnEB9g4vA"
   }
 }`;
 
