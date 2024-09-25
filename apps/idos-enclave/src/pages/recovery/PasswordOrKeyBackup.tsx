d1a301b23892a679c1806d7195bbdd276c3dd9e1
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2024-09-25 14:56:52 +0300

    feat: connect ethers to wallet

--- apps/idos-enclave/src/pages/recovery/PasswordOrKeyBackup.tsx
+++ apps/idos-enclave/src/pages/recovery/PasswordOrKeyBackup.tsx
@@ -307,6 +307,7 @@ export function PasswordOrKeyBackup({
   const storeWithLit = async () => {
     status.value = "pending";
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
     const signer = provider.getSigner();
     const address = await signer.getAddress();
 
