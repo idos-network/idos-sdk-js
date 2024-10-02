357e8f5e714e9c0d2b738eabc30ea49a84f32bb9
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2024-10-02 13:31:26 +0300

    chore: cleanup vitest

--- packages/issuer-sdk-js/src/env.d.ts
+++ packages/issuer-sdk-js/src/env.d.ts
@@ -2,15 +2,5 @@ interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
 
-interface ImportMetaEnv {
-  readonly VITE_IDOS_ENCLAVE_URL: string;
-  readonly VITE_IDOS_NODE_URL: string;
-  readonly VITE_IDOS_NODE_KWIL_DB_ID: string;
-  readonly VITE_IDOS_NODE_KWIL_CHAIN_ID: string;
-  readonly VITE_IDOS_EVM_DEFAULT_CONTRACT_ADDRESS: string;
-  readonly VITE_IDOS_EVM_DEFAULT_CHAIN_ID: string;
-  readonly VITE_IDOS_NEAR_DEFAULT_NETWORK: string;
-  readonly VITE_IDOS_NEAR_DEFAULT_CONTRACT_ID: string;
-  readonly VITE_IDOS_NEAR_DEFAULT_RPC_URL: string;
-  readonly VITE_FRACTAL_ID_URL: string;
-}
+// biome-ignore lint/complexity/noBannedTypes: Its fine to have an empty env object for now.
+type ImportMetaEnv = {};
--- packages/issuer-sdk-js/vitest.config.ts
+++ packages/issuer-sdk-js/vitest.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
-    environment: "jsdom",
   },
 });
