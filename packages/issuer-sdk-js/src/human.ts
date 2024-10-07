8bde74970b17592fd56792a69aaeaca96e6bbb7f
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2024-10-07 11:48:44 +0300

    feat(issuer): create a human functionality

--- packages/issuer-sdk-js/src/human.ts
+++ packages/issuer-sdk-js/src/human.ts
@@ -2,18 +2,17 @@ import type { idOSHuman, idOSWallet } from "@idos-network/idos-sdk-types";
 import type { CreateIssuerConfig } from "./create-issuer-config";
 import { createActionInput } from "./internal";
 
-interface CreateProfileReqParams extends Omit<idOSHuman, "id"> {}
+interface CreateProfileReqParams extends idOSHuman {}
 
-export async function createHumanProfile(
+async function createHumanProfile(
   { dbid, kwilClient, signer }: CreateIssuerConfig,
   params: CreateProfileReqParams,
 ) {
-  const id = crypto.randomUUID();
   const response = await kwilClient.execute(
     {
       name: "add_human_as_inserter",
       dbid,
-      inputs: [createActionInput({ ...params, id })],
+      inputs: [createActionInput(params)],
     },
     signer,
     true,
@@ -22,22 +21,60 @@ export async function createHumanProfile(
   return response.data?.tx_hash;
 }
 
-interface CreateWalletReqParams extends Omit<idOSWallet, "id"> {}
+interface UpsertWalletReqParams extends idOSWallet {}
 
-export async function createWallet(
+async function upsertWallet(
   { dbid, kwilClient, signer }: CreateIssuerConfig,
-  params: CreateWalletReqParams,
+  params: UpsertWalletReqParams,
 ) {
-  const id = crypto.randomUUID();
   const response = await kwilClient.execute(
     {
       name: "upsert_wallet_as_inserter",
       dbid,
-      inputs: [createActionInput({ ...params, id })],
+      inputs: [createActionInput(params)],
     },
     signer,
     true,
   );
 
   return response.data?.tx_hash;
+}
+
+export async function createHuman(
+  config: CreateIssuerConfig,
+  current_public_key: string,
+  wallet: Omit<UpsertWalletReqParams, "id" | "human_id">,
+) {
+  const human_id = crypto.randomUUID();
+  const wallet_id = crypto.randomUUID();
+
+  const humanReqParams = {
+    current_public_key,
+    id: human_id,
+  };
+
+  const human_tx_hash = await createHumanProfile(config, humanReqParams);
+
+  const walletReqParams = {
+    ...wallet,
+    human_id,
+    id: wallet_id,
+  };
+
+  const wallet_tx_hash = await upsertWallet(config, walletReqParams);
+
+  return [
+    {
+      tx_hash: human_tx_hash,
+      data: {
+        ...humanReqParams,
+      },
+    },
+    {
+      tx_hash: wallet_tx_hash,
+      data: {
+        ...walletReqParams,
+      },
+    },
+  ];
 }
