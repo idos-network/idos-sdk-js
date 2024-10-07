3bb66f09c1747de784971de980c716d20178b5f7
ditoglez <ditoglez@gmail.com>
2024-10-07 12:05:01 +0300

    feat(issuer): create a human functionality (#306)

--- packages/issuer-sdk-js/src/internal.ts
+++ packages/issuer-sdk-js/src/internal.ts
@@ -1,6 +1,7 @@
 import { Utils } from "@kwilteam/kwil-js";
 
-export function createActionInput(params: Record<string, string>): Utils.ActionInput {
+// biome-ignore lint/suspicious/noExplicitAny: Using `any` is fine over here as we don't need to be strict about types being passed in.
+export function createActionInput(params: Record<string, any>): Utils.ActionInput {
   const prefixedEntries = Object.entries(params).map(([key, value]) => [`$${key}`, value]);
   const prefixedObject = Object.fromEntries(prefixedEntries);
   return Utils.ActionInput.fromObject(prefixedObject);
