ef967731b8eae563ccae94e643c715e58402d05c
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2024-10-16 10:44:22 +0300

    chore: extract nep413-signer to a new package

--- packages/idos-sdk-server-dapp/src/idOS-grantee.ts
+++ packages/idos-sdk-server-dapp/src/idOS-grantee.ts
@@ -1,10 +1,7 @@
+import { implicitAddressFromPublicKey, kwilNep413Signer } from "@idos-network/kwil-nep413-signer";
 import { KwilSigner, NodeKwil } from "@kwilteam/kwil-js";
 import * as Base64Codec from "@stablelib/base64";
-import * as BinaryCodec from "@stablelib/binary";
-import * as BytesCodec from "@stablelib/bytes";
-import * as sha256 from "@stablelib/sha256";
 import * as Utf8Codec from "@stablelib/utf8";
-import * as BorshCodec from "borsh";
 import type { ethers } from "ethers";
 import type { KeyPair } from "near-api-js";
 import nacl from "tweetnacl";
@@ -17,61 +14,7 @@ import {
 import type { GrantChild } from "../../idos-sdk-js/src/lib/grants/grant-child.ts";
 import type Grant from "../../idos-sdk-js/src/lib/grants/grant.ts";
 import { KwilWrapper } from "../../idos-sdk-js/src/lib/kwil-wrapper.ts";
-import { assertNever, implicitAddressFromPublicKey } from "../../idos-sdk-js/src/lib/utils.ts";
-
-const kwilNep413Signer =
-  (recipient: string) =>
-  (keyPair: KeyPair) =>
-  async (messageBytes: Uint8Array): Promise<Uint8Array> => {
-    const message = Utf8Codec.decode(messageBytes);
-    const nonceLength = 32;
-    const nonce = crypto.getRandomValues(new Uint8Array(nonceLength));
-
-    const nep413BorschSchema = {
-      struct: {
-        message: "string",
-        nonce: { array: { type: "u8", len: nonceLength } },
-        recipient: "string",
-        callbackUrl: { option: "string" },
-      },
-    } as const;
-
-    const tag = 2147484061; // 2**31 + 413
-
-    const { signature } = keyPair.sign(
-      sha256.hash(
-        BytesCodec.concat(
-          BorshCodec.serialize("u32", tag),
-          BorshCodec.serialize(nep413BorschSchema, { message, nonce, recipient }),
-        ),
-      ),
-    );
-
-    const kwilNep413BorschSchema = {
-      struct: {
-        tag: "u32",
-        ...nep413BorschSchema.struct,
-      },
-    };
-
-    const kwilNep413BorshParams = {
-      tag,
-      message,
-      nonce,
-      recipient,
-    };
-
-    const kwilNep413BorshPayload = BorshCodec.serialize(
-      kwilNep413BorschSchema,
-      kwilNep413BorshParams,
-    );
-
-    return BytesCodec.concat(
-      BinaryCodec.writeUint16BE(kwilNep413BorshPayload.length),
-      kwilNep413BorshPayload,
-      signature,
-    );
-  };
+import { assertNever } from "../../idos-sdk-js/src/lib/utils.ts";
 
 export class NoncedBox {
   keyPair: nacl.BoxKeyPair;
