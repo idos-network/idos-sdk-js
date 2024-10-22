013d28a802a50fa860a7286058d8f38894ef7e87
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2024-10-22 15:54:55 +0300

    chore: types version

--- packages/issuer-sdk-js/src/index.ts
+++ packages/issuer-sdk-js/src/index.ts
@@ -1,5 +1,3 @@
 export * from "./create-issuer-config";
 export * from "./credentials";
 export * from "./human";
-
-export type { idOSCredential, idOSHuman, idOSWallet } from "@idos-network/idos-sdk-types";
