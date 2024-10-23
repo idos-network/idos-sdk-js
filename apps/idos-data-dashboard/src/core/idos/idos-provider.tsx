083f8905e45cfbc8d7a5827f3dfd138f2afaa5f6
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2024-10-23 16:56:42 +0300

    remove window._sdk

--- apps/idos-data-dashboard/src/core/idos/idos-provider.tsx
+++ apps/idos-data-dashboard/src/core/idos/idos-provider.tsx
@@ -55,8 +55,6 @@ export const Provider = ({ children }: PropsWithChildren) => {
         url: import.meta.env.VITE_IDOS_ENCLAVE_URL,
       },
     });
-    //@ts-ignore
-    window._sdk = _sdk;
 
     const profile = await _sdk.hasProfile(userAddress);
     setHasProfile(profile);
