73f783c1ecdce75a1d3fc337a4ebc54920546cdd
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2024-12-16 16:44:32 +0200

    handle no public encryptoion key found

--- apps/idos-enclave/src/pages/App.tsx
+++ apps/idos-enclave/src/pages/App.tsx
@@ -63,7 +63,7 @@ export function App({ store, enclave }: AppProps) {
   // Confirm options.
   const [origin, setOrigin] = useState<string | null>(null);
   const [message, setMessage] = useState<string | null>(null);
-  const [encryptionPublicKey, setEncryptionUserPublicKey] = useState<string | undefined>();
+  const [encryptionPublicKey, setEncryptionUserPublicKey] = useState<string>("");
   const [humanId] = useState<string | null>(
     new URLSearchParams(window.location.search).get("humanId"),
   );
@@ -88,11 +88,6 @@ export function App({ store, enclave }: AppProps) {
     }
   }, [theme]);
 
-  useEffect(() => {
-    if (mode === "new" || !responsePort.current) return;
-    if (!encryptionPublicKey) onError("Can’t find a public encryption key for this user");
-  }, [mode, encryptionPublicKey, responsePort.current]);
-
   const resetMethod = useCallback(() => setMethod(null), []);
 
   /**
@@ -107,11 +102,11 @@ export function App({ store, enclave }: AppProps) {
       throw new Error(`Unexpected request from parent: ${requestData.intent}`);
 
     responsePort.current = ports[0];
+    setEncryptionUserPublicKey(event.data.message.expectedUserEncryptionPublicKey);
 
     switch (requestData.intent) {
       case "auth":
         setMethod(null);
-        setEncryptionUserPublicKey(event.data.message.expectedUserEncryptionPublicKey);
         break;
 
       case "passkey":
@@ -180,6 +175,11 @@ export function App({ store, enclave }: AppProps) {
     onSuccess,
     mode,
   };
+
+  useEffect(() => {
+    if (mode === "new" || !responsePort.current) return;
+    if (!encryptionPublicKey) onError("Can’t find a public encryption key for this user");
+  }, [mode, encryptionPublicKey, responsePort.current]);
 
   if (confirm && message) {
     return (
