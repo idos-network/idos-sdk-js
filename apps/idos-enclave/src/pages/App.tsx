f400ec7f07f4c2e5f5083f0b2facd259800e7f18
Mo <51370781+Mohammed-Mamoun98@users.noreply.github.com>
2024-12-16 16:46:29 +0200

    always pass EPEK to enclave wether it remembers auth method or not (#387)
    Co-authored-by: web the third <hi@whoisjuliosantos.com>

--- apps/idos-enclave/src/pages/App.tsx
+++ apps/idos-enclave/src/pages/App.tsx
@@ -63,7 +63,7 @@ export function App({ store, enclave }: AppProps) {
   // Confirm options.
   const [origin, setOrigin] = useState<string | null>(null);
   const [message, setMessage] = useState<string | null>(null);
-  const [encryptionPublicKey, setEncryptionUserPublicKey] = useState<string | undefined>();
+  const [encryptionPublicKey, setEncryptionUserPublicKey] = useState<string>("");
   const [humanId] = useState<string | null>(
     new URLSearchParams(window.location.search).get("humanId"),
   );
@@ -107,11 +107,11 @@ export function App({ store, enclave }: AppProps) {
       throw new Error(`Unexpected request from parent: ${requestData.intent}`);
 
     responsePort.current = ports[0];
+    setEncryptionUserPublicKey(requestData.message?.expectedUserEncryptionPublicKey);
 
     switch (requestData.intent) {
       case "auth":
         setMethod(null);
-        setEncryptionUserPublicKey(event.data.message.expectedUserEncryptionPublicKey);
         break;
 
       case "passkey":
@@ -180,6 +180,11 @@ export function App({ store, enclave }: AppProps) {
     onSuccess,
     mode,
   };
+
+  useEffect(() => {
+    if (mode === "new" || !responsePort.current) return; // encryptionPublicKey is only set after responsePort is set
+    if (!encryptionPublicKey) onError("can't find a public encryption key for this user");
+  }, [mode, encryptionPublicKey, responsePort.current]);
 
   if (confirm && message) {
     return (
