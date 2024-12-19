5867114de670a86a8b654eca7057ee20ab65e15c
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2024-12-19 20:47:40 +0200

    chore(grantee-sdk-demo): bootstrap app

---
+++ examples/grantee-sdk-demo/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="grid h-dvh place-content-center">
+      <h1 className="font-semibold text-4xl">Grantee SDK Demo</h1>
+    </div>
+  );
+}
