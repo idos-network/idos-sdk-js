763cfa480579b95498974a409ecc3557e9ce8185
Paulo Koch <pkoch@idos.network>
2025-01-08 18:04:48 +0000

    feat: add wallet connect

--- apps/dashboard-for-dapps/src/routes/__root.tsx
+++ apps/dashboard-for-dapps/src/routes/__root.tsx
@@ -1,4 +1,6 @@
 import { Button } from "@/components/ui";
+import { Provider } from "@/idOS.provider";
+import { injectedConnector, walletConnectConnector } from "@/wagmi.config";
 import {
   Center,
   Container,
@@ -14,11 +16,8 @@ import { type QueryClient, useQueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Link, Outlet, createRootRouteWithContext, useNavigate } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { useAccount, useConnect, useDisconnect } from "wagmi";
-import { injected } from "wagmi/connectors";
-
-import { Provider } from "@/idOS.provider";
 import { useEffect } from "react";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
 
 export const Route = createRootRouteWithContext<{
   queryClient: QueryClient;
@@ -38,16 +37,18 @@ function ConnectWallet() {
   return (
     <Center h="100dvh" flexDirection="column" gap="4">
       <Heading fontSize="xl">Connect your wallet to continue</Heading>
-      <Button
-        loading={isPending}
-        onClick={() =>
-          connect({
-            connector: injected(),
-          })
-        }
-      >
-        Connect your wallet
-      </Button>
+      {isPending ? (
+        <>
+          <Button loading={true}> Waiting wallet </Button>
+        </>
+      ) : (
+        <>
+          <Button onClick={() => connect({ connector: injectedConnector })}>Browser wallet</Button>
+          <Button onClick={() => connect({ connector: walletConnectConnector })}>
+            WalletConnect
+          </Button>
+        </>
+      )}
     </Center>
   );
 }
