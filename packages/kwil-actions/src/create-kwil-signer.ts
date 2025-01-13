3b125d22fff55a360f94eadd8da4ce122c686dc7
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2025-01-13 20:42:01 +0200

    feat(kiwl-actions): check for a NEAR keypair

--- packages/kwil-actions/src/create-kwil-signer.ts
+++ packages/kwil-actions/src/create-kwil-signer.ts
@@ -1,7 +1,7 @@
 import { implicitAddressFromPublicKey, kwilNep413Signer } from "@idos-network/kwil-nep413-signer";
 import { KwilSigner } from "@kwilteam/kwil-js";
 import { Wallet } from "ethers";
-import { KeyPair } from "near-api-js";
+import type { KeyPair } from "near-api-js";
 import nacl from "tweetnacl";
 
 /**
@@ -20,32 +20,57 @@ function isNaclSignKeyPair(object: unknown): object is nacl.SignKeyPair {
   );
 }
 
-type KwilSignerType = KeyPair | Wallet | nacl.SignKeyPair;
+/**
+ * Helper function to check if the given object is a NEAR KeyPair.
+ */
+function isNearKeyPair(object: unknown): object is KeyPair {
+  return (
+    object !== null &&
+    typeof object === "object" &&
+    "getPublicKey" in object &&
+    "sign" in object &&
+    typeof object.getPublicKey === "function" &&
+    typeof object.sign === "function"
+  );
+}
 
-export function createKwilSigner(signer: KwilSignerType): KwilSigner {
+type KwilSignerType = KeyPair | Wallet | nacl.SignKeyPair;
+type SignerAddress = string;
+
+/**
+ * Helper function to create a `KwilSigner` and its associated `SignerAddress`.
+ * Useful for creating a `KwilSigner` and its associated `SignerAddress` for the `idOSGrantee` constructor.
+ */
+export function createKwilSigner(signer: KwilSignerType): [KwilSigner, SignerAddress] {
   if (isNaclSignKeyPair(signer)) {
-    return new KwilSigner(
-      async (msg: Uint8Array) => nacl.sign.detached(msg, signer.secretKey),
-      signer.publicKey,
-      "ed25519",
-    );
+    return [
+      new KwilSigner(
+        async (msg: Uint8Array) => nacl.sign.detached(msg, signer.secretKey),
+        signer.publicKey,
+        "ed25519",
+      ),
+      implicitAddressFromPublicKey(signer.publicKey.toString()),
+    ];
   }
 
-  if (signer instanceof KeyPair) {
-    return new KwilSigner(
-      kwilNep413Signer("idos-issuer")(signer),
+  if (isNearKeyPair(signer)) {
+    return [
+      new KwilSigner(
+        kwilNep413Signer("idos-issuer")(signer),
+        implicitAddressFromPublicKey(signer.getPublicKey().toString()),
+        "nep413",
+      ),
       implicitAddressFromPublicKey(signer.getPublicKey().toString()),
-      "nep413",
-    );
+    ];
   }
 
   if (signer instanceof Wallet) {
-    return new KwilSigner(signer, signer.address);
+    return [new KwilSigner(signer, signer.address), signer.address];
   }
 
   // Force the check that `signer` is never.
   // If these lines start complaining, that means we're missing an `if` above.
   return ((_: never) => {
-    throw new Error("Invalid signer type");
+    throw new Error("Invalid `signer` type");
   })(signer);
 }
