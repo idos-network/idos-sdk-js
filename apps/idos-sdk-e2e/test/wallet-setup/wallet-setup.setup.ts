11e3ccaa4f00a4343b90501f652652d702875dff
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2025-01-16 12:31:44 +0200

    fix(idos-sdk-e2e): upgrade synpress to latest version

--- apps/idos-sdk-e2e/test/wallet-setup/wallet-setup.setup.ts
+++ apps/idos-sdk-e2e/test/wallet-setup/wallet-setup.setup.ts
@@ -1,11 +1,11 @@
-import { MetaMask, defineWalletSetup, getExtensionId } from "@synthetixio/synpress";
+import { defineWalletSetup } from "@synthetixio/synpress-cache";
+import { MetaMask, getExtensionId } from "@synthetixio/synpress/playwright";
 
 const SEED_PHRASE = process.env.WALLET_SEED_PHRASE;
 const PASSWORD = process.env.WALLET_PASSWORD;
 
 export default defineWalletSetup(PASSWORD, async (context, walletPage) => {
   const extensionId = await getExtensionId(context, "MetaMask");
-
   const metamask = new MetaMask(context, walletPage, PASSWORD, extensionId);
 
   await metamask.importWallet(SEED_PHRASE);
--- apps/idos-sdk-e2e/tests/02-data.spec.ts
+++ apps/idos-sdk-e2e/tests/02-data.spec.ts
@@ -1,5 +1,6 @@
-import { MetaMask, metaMaskFixtures, testWithSynpress } from "@synthetixio/synpress";
-import basicSetup from "./wallet-setup/basic.setup";
+import { testWithSynpress } from "@synthetixio/synpress";
+import { MetaMask, metaMaskFixtures } from "@synthetixio/synpress/playwright";
+import basicSetup from "../test/wallet-setup/wallet-setup.setup";
 
 const test = testWithSynpress(metaMaskFixtures(basicSetup));
 
