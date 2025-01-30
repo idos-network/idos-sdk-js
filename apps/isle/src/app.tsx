a2bd316e6305d8d884b0f6cfa7719328557c024f
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2025-01-30 21:42:42 +0200

    chore(isle): bootstrap the app

---
+++ apps/isle/src/app.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div class="grid min-h-dvh place-content-center">
+      <h1 class="font-semibold text-3xl">idOS Isle</h1>
+    </div>
+  );
+}
---
+++ apps/isle/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // This enables dark mode with the 'dark' class
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
