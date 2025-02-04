f608c7ef50a355d07d994220304f89f4c9ee3be2
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2025-02-04 21:24:22 +0200

    PR comments handled

--- apps/isle/src/components/stepper.tsx
+++ apps/isle/src/components/stepper.tsx
@@ -1,30 +1,38 @@
-import { tv } from "tailwind-variants";
+import { tv, cn as _cn } from "tailwind-variants";
 
-const stepOutterCircleVariants = tv({
-  base: "relative h-6 w-6 rounded-full ",
+const cn = (...args: any) => _cn(args)({ twMerge: true });
+
+const stepCircleVariants = tv({
+  slots: {
+    outerCircle: "relative h-6 w-6 rounded-full",
+    innerCircle: "-translate-x-1/2 -translate-y-1/2 absolute top-1/2 left-1/2 h-2 w-2 rounded-full",
+  },
   variants: {
     active: {
-      on: "bg-emerald-900",
-      off: "bg-neutral-800",
+      on: {
+        outerCircle: "bg-emerald-900",
+        innerCircle: "bg-teal-400",
+      },
+      off: {
+        outerCircle: "bg-neutral-800",
+        innerCircle: "bg-neutral-500",
+      },
     },
   },
 });
 
-const stepInnerCircleVariants = tv({
-  base: "-translate-x-1/2 -translate-y-1/2 absolute absolute top-1/2 left-1/2 h-2 w-2 rounded-full",
-  variants: {
-    active: {
-      on: "bg-teal-400",
-      off: "bg-neutral-500",
-    },
-  },
-});
+export interface StepCircleProps {
+  active: boolean;
+}
 
-const StepCircle = ({ active }: { active: boolean }) => (
-  <div className={stepOutterCircleVariants({ active: active ? "on" : "off" })}>
-    <div className={stepInnerCircleVariants({ active: active ? "on" : "off" })} />
-  </div>
-);
+const StepCircle = ({ active }: StepCircleProps) => {
+  const { outerCircle, innerCircle } = stepCircleVariants({ active: active ? "on" : "off" });
+  return (
+    <div className={outerCircle()}>
+      <div className={innerCircle()} />
+    </div>
+  );
+};
 
 interface Step {
   index?: number;
@@ -33,10 +41,15 @@ interface Step {
   render: React.ReactNode;
 }
 
+interface StepperProps {
+  steps: Step[];
+  index: number;
+}
+
 export function Stepper({
   index: activeIndex = -1, // stepper starts with index -1 to hide the title
   steps,
-}: { steps: Step[]; index: number }) {
+}: StepperProps) {
   // showing the current step will hide the stepper general title and make the stepper horizontal
   const currentStep = steps[activeIndex];
 
@@ -44,14 +57,21 @@ export function Stepper({
     <>
       {currentStep && (
         <div className="my-6 flex w-full justify-center">
-          <h2 className="text-center font-semibold text-lg leading-md">{currentStep?.title}</h2>
+          <h2 className="text-center font-semibold text-lg leading-md">{currentStep.title}</h2>
         </div>
       )}
       <div
-        className={`relative mx-auto flex max-w-[206px] ${currentStep ? "flex-row justify-between" : "flex-col"} items-start gap-5.5`}
+        className={cn(
+          "relative mx-auto flex max-w-[206px] items-start gap-5.5",
+          currentStep ? "flex-row justify-between" : "flex-col",
+        )}
       >
         <div
-          className={`absolute ${currentStep ? "top-[12px] left-0 min-h-[1px] w-full" : "left-[12px] h-full min-w-[1px]"} bg-neutral-500`}
+          className={cn(
+            "absolute",
+            currentStep ? "top-[12px] left-0 min-h-[1px] w-full" : "left-[12px] h-full min-w-[1px]",
+            "bg-neutral-500",
+          )}
         />
         {steps.map((step, index) => (
           <div key={step.index} className="flex items-center gap-2">
