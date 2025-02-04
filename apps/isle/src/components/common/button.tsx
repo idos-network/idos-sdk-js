31e9b45aeb4d69b7ee02afdcae312ef23a1217ce
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2025-02-04 19:58:51 +0200

    PR comments handled

--- apps/isle/src/components/common/button.tsx
+++ apps/isle/src/components/common/button.tsx
@@ -1,16 +1,20 @@
-import type { ButtonHTMLAttributes } from "react";
+import type { JSX } from "preact";
 import { tv } from "tailwind-variants";
 
-const buttonVariants = tv({
-  base: "cursor-pointer bg-aquamarine-400 hover:bg-aquamarine-500 disabled:bg-neutral-300 min-h-[40px] text-sm px-4 rounded-md text-neutral-950 grid place-items-center font-medium leading-md w-fit",
+const button = tv({
+  base: "cursor-pointer bg-primary hover:bg-primary-100 disabled:bg-neutral-300 min-h-[40px] text-sm px-4 rounded-md text-neutral-950 grid place-items-center font-medium leading-md w-fit",
   defaultVariants: {
     color: "primary",
   },
 });
 
-export function Button({ className, children, ...props }: ButtonHTMLAttributes) {
+export function Button({
+  class: _class,
+  children,
+  ...props
+}: JSX.HTMLAttributes<HTMLButtonElement>) {
   return (
-    <button className={[buttonVariants(), className].join(" ")} type="button" {...props}>
+    <button className={[button({ class: _class as string })].join(" ")} type="button" {...props}>
       {children}
     </button>
   );
