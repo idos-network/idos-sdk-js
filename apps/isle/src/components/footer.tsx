7ff056788c43d0b2f0006a9154f188622ddb81b1
ditoglez <ditoglez@gmail.com>
2025-02-07 12:25:08 +0200

    feat(isle): add footer component (#513)

---
+++ apps/isle/src/components/footer.tsx
@@ -0,0 +1,19 @@
+import { Image, Text, chakra } from "@chakra-ui/react";
+
+export function Footer() {
+  return (
+    <chakra.footer>
+      <Text
+        display={{ base: "none", sm: "flex" }}
+        color="gray.500"
+        fontSize="sm"
+        textAlign="center"
+        placeContent="center"
+        alignItems="center"
+        gap="2"
+      >
+        Powered by <Image src="/footer-logo.svg" alt="Powered by idOS" />
+      </Text>
+    </chakra.footer>
+  );
+}
