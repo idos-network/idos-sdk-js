144b12a75f30dfe94b318b5ca18f5ef154d9c51f
Paulo Koch <pkoch@idos.network>
2025-02-24 18:42:01 +0000

    feat: add server and client exports

--- packages/issuer-sdk-js/tsup.config.ts
+++ packages/issuer-sdk-js/tsup.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: [
     "./src/index.ts",
+    "./src/client/index.ts",
+    "./src/server/index.ts",
     "./src/create-issuer-config.ts",
     "./src/user.ts",
     "./src/credentials.ts",
