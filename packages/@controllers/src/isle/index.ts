668add6a055960f1b4f86eb12b7ac6f31e55c792
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2025-03-13 19:21:44 +0200

    chore(CI-app): update isle url at Consumer&Issuer Demo app

--- packages/@controllers/src/isle/index.ts
+++ packages/@controllers/src/isle/index.ts
@@ -179,7 +179,7 @@ export const createIsleController = (options: idOSIsleControllerOptions): idOSIs
   let iframe: HTMLIFrameElement | null = null;
   let enclave: EnclaveProvider | null = null;
   const controller: Controller = createController({
-    targetOrigin: "https://localhost:5174",
+    targetOrigin: "https://isle.idos.network/",
   });
   let channel: ChannelInstance<IsleControllerMessage, IsleNodeMessage> | null = null;
   let signer: JsonRpcSigner | undefined;
@@ -659,7 +659,7 @@ export const createIsleController = (options: idOSIsleControllerOptions): idOSIs
   iframe = document.createElement("iframe");
   iframe.id = iframeId;
   // @todo: make the domain environment aware.
-  iframe.src = "https://localhost:5174";
+  iframe.src = "https://isle.idos.network/";
   iframe.style.width = "100%";
   iframe.style.height = "100%";
   iframe.style.border = "none";
