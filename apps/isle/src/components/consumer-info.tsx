6c365f89ff53599a47ef22b47dd893bb2765f0cf
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2025-03-20 10:02:15 +0200

    feat(isle): ui imprvements for images

--- apps/isle/src/components/consumer-info.tsx
+++ apps/isle/src/components/consumer-info.tsx
@@ -8,7 +8,16 @@ interface ConsumerInfoProps {
 export function ConsumerInfo({ name, logo }: ConsumerInfoProps) {
   return (
     <HStack gap="2.5" alignItems="flex-start">
-      <Image src={logo} alt={name} width="30px" height="30px" rounded="full" />
+      <Image
+        src={logo}
+        alt={name}
+        width="30px"
+        height="30px"
+        rounded="full"
+        aspectRatio="square"
+        objectFit="contain"
+        border="1px solid {colors.gray.200}"
+      />
       <Text>
         <Text as="span" fontWeight="medium" fontSize="lg">
           {name}
