db1d5c493eb6053c2de9b5a8afaeb15d27fed59f
Mo <51370781+Mohammed-Mamoun98@users.noreply.github.com>
2025-04-01 22:32:53 +0200

    refactor(passporting-demo): better approach for finding dag with user_id (#720)

--- examples/passporting/src/app/api/shared-credential/[userId]/route.ts
+++ examples/passporting/src/app/api/shared-credential/[userId]/route.ts
@@ -3,20 +3,20 @@ import { getGrants } from "@idos-network/core/kwil-actions";
 
 export async function GET(request: Request, { params }: { params: Promise<{ userId: string }> }) {
   const userId = (await params).userId;
-
   const consumer = await idOSConsumer();
-  // @todo: filter grants by user id
-  const grants = await getGrants(consumer.kwilClient, 1);
-  const credentials = grants.find((grant) => grant.ag_owner_user_id === userId);
 
-  if (!credentials) {
+  const grants = await getGrants(consumer.kwilClient, {
+    user_id: userId,
+  });
+  const grant = grants.find((grant) => grant.ag_owner_user_id === userId);
+
+  if (!grant) {
     return new Response(JSON.stringify(null), {
       status: 200,
     });
   }
 
-  const credential = await consumer.getReusableCredentialCompliantly(credentials.data_id);
-
+  const credential = await consumer.getReusableCredentialCompliantly(grant.data_id);
   if (!credential) {
     return new Response(JSON.stringify(null), {
       status: 200,
--- packages/@core/src/kwil-actions/grants.ts
+++ packages/@core/src/kwil-actions/grants.ts
@@ -14,11 +14,21 @@ export async function getGrantsCount(kwilClient: KwilActionClient) {
 /**
  * Returns the Access Grants for the given `signer` in a paginated manner.
  */
-export async function getGrants(kwilClient: KwilActionClient, page = 1, size = 7) {
+
+interface GetGrantsParams {
+  page?: number;
+  size?: number;
+  user_id?: string | null;
+}
+
+export async function getGrants(
+  kwilClient: KwilActionClient,
+  params: GetGrantsParams = { page: 1, size: 10, user_id: null },
+) {
   //@todo: add pagination values to the response
   return kwilClient.call<idOSGrant[]>({
     name: "get_access_grants_granted",
-    inputs: { page, size },
+    inputs: params,
   });
 }
 export interface CreateAccessGrantByDAGParams {
