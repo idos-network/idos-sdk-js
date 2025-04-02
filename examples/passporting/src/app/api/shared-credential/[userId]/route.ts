bf036548145ecb1fb5e2ba27648e431223f0534f
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2025-04-02 15:59:33 +0200

    fix(passporting): getting credential complintly issue

--- examples/passporting/src/app/api/shared-credential/[userId]/route.ts
+++ examples/passporting/src/app/api/shared-credential/[userId]/route.ts
@@ -11,19 +11,19 @@ export async function GET(request: Request, { params }: { params: Promise<{ user
   const grant = grants.find((grant) => grant.ag_owner_user_id === userId);
 
   if (!grant) {
-    return new Response(JSON.stringify(null), {
+    return new Response(JSON.stringify({ credential: null, cause: "no-grant" }), {
       status: 200,
     });
   }
 
   const credential = await consumer.getReusableCredentialCompliantly(grant.data_id);
   if (!credential) {
-    return new Response(JSON.stringify(null), {
+    return new Response(JSON.stringify({ credential: null, cause: "no-credential" }), {
       status: 200,
     });
   }
 
-  return new Response(JSON.stringify(credential), {
+  return new Response(JSON.stringify({ credential, cause: "success" }), {
     status: 200,
   });
 }
