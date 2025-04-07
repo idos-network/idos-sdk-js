ff439fe47ec8dbf1d7b2260fea988d8d26cf8be8
Mo <51370781+Mohammed-Mamoun98@users.noreply.github.com>
2025-04-07 21:18:32 +0200

    chore(ci-demo): abort passporting service on failure (#759)
    Co-authored-by: Paulo Koch <pkoch@idos.network>

--- examples/consumer-and-issuer/src/actions/index.ts
+++ examples/consumer-and-issuer/src/actions/index.ts
@@ -1,6 +1,6 @@
 "use server";
 import { getIssuerServerConfig } from "@/issuer.config";
-import { base64Decode, base64Encode, toBytes } from "@idos-network/core";
+import { base64Decode, base64Encode, idOSCredential, toBytes } from "@idos-network/core";
 import {
   createCredentialByDelegatedWriteGrant,
   createUser,
@@ -247,21 +247,56 @@ export const getCredentialCompliantly = async (credentialId: string) => {
   return credential;
 };
 
-export const invokePassportingService = async (payload: unknown) => {
+type PassportingServiceResponse =
+  | { success: true; data: { dag_data_id: string } }
+  | {
+      success: false;
+      error: {
+        cause?: unknown;
+        message: string;
+      };
+    };
+
+export const invokePassportingService = async (
+  payload: unknown,
+): Promise<PassportingServiceResponse> => {
   invariant(process.env.PASSPORTING_SERVICE_URL, "`PASSPORTING_SERVICE_URL` is not set");
-  return await await fetch(process.env.PASSPORTING_SERVICE_URL ?? "", {
-    method: "POST",
-    body: JSON.stringify(payload),
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.PASSPORTING_SERVICE_API_KEY}`,
-    },
-  })
-    .then((res) => res.json())
-    .catch((err) => {
-      console.error(err);
-      return { ok: false, error: err };
+
+  try {
+    const response = await fetch(process.env.PASSPORTING_SERVICE_URL, {
+      method: "POST",
+      body: JSON.stringify(payload),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.PASSPORTING_SERVICE_API_KEY}`,
+      },
     });
+
+    const data = (await response.json()) as PassportingServiceResponse;
+
+    if (!response.ok) {
+      return { success: false, error: { message: response.statusText } };
+    }
+
+    if (!data.success) {
+      return { success: false, error: data.error };
+    }
+
+    return { success: true, data: data.data };
+  } catch (error) {
+    console.error(error);
+    if (error instanceof Error) {
+      return {
+        success: false,
+        error: { cause: error.cause, message: error.message },
+      };
+    }
+
+    return {
+      success: false,
+      error: { message: JSON.stringify(error) },
+    };
+  }
 };
 
 export const generateKrakenUrlToken = async () => {
