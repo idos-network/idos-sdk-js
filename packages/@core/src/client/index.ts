699c87c00cc121902620ac0d14d0da317e943987
Paulo Koch <pkoch@idos.network>
2025-04-08 19:28:52 +0100

    fix: don't clear the enclave on logoff (#773)

--- packages/@core/src/client/index.ts
+++ packages/@core/src/client/index.ts
@@ -134,7 +134,6 @@ export class idOSClientWithUserSigner implements Omit<Properties<idOSClientIdle>
 
   async logOut(): Promise<idOSClientIdle> {
     this.kwilClient.setSigner(undefined);
-    await this.enclaveProvider.reset();
     return new idOSClientIdle(this.store, this.kwilClient, this.enclaveProvider);
   }
 
@@ -182,7 +181,6 @@ export class idOSClientLoggedIn implements Omit<Properties<idOSClientWithUserSig
 
   async logOut(): Promise<idOSClientIdle> {
     this.kwilClient.setSigner(undefined);
-    await this.enclaveProvider.reset();
     return new idOSClientIdle(this.store, this.kwilClient, this.enclaveProvider);
   }
 
