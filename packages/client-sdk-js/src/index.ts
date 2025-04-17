c020732df09ac08f8d27788d9f1e5951b3059edc
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2025-04-17 11:54:38 +0300

    chore(client-sdk): export as factory function

--- packages/client-sdk-js/src/index.ts
+++ packages/client-sdk-js/src/index.ts
@@ -1,9 +1,9 @@
-export {
-  type idOSClient,
-  idOSClientConfiguration,
-  idOSClientIdle,
-  idOSClientWithUserSigner,
-  idOSClientLoggedIn,
-} from "@idos-network/core";
+import { idOSClientConfiguration } from "@idos-network/core";
+
+export type { idOSClient } from "@idos-network/core";
 
 export * from "@idos-network/core/types";
+
+export function createIOSClient(config: idOSClientConfiguration) {
+  return new idOSClientConfiguration(config);
+}
