af3b6250b563b1a7a47e54aec22b2ec931a242f7
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2025-04-24 09:20:01 +0300

    chore: cleanup old approach

--- packages/issuer-sdk-js/src/utils.ts
+++
@@ -1,8 +0,0 @@
-export function ensureEntityId<T extends { id?: string }>(
-  entity: T,
-): Omit<T, "id"> & { id: string } {
-  return {
-    ...entity,
-    id: entity.id ?? crypto.randomUUID(),
-  };
-}
