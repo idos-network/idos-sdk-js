2533dc2828ae558583fffad6c009cb2eab0771f5
Mo <51370781+Mohammed-Mamoun98@users.noreply.github.com>
2025-05-12 21:35:15 +0300

    feat(e2e): better cleanup for dummy wallets (#861)

--- apps/idos-sdk-e2e/specs/02-data.spec.ts
+++ apps/idos-sdk-e2e/specs/02-data.spec.ts
@@ -15,8 +15,6 @@ test.beforeEach(async ({ context, page }) => {
   await page.evaluate(() => window.localStorage.clear());
 });
 
-const TEST_WALLET_ADDRESS = "0xB5B3a244943E5A64511673528e003BE79B18901d";
-
 test("should fetch credentials successfully", async ({
   context,
   page,
@@ -34,54 +32,94 @@ test("should fetch credentials successfully", async ({
   await expect(list.getByRole("listitem")).toHaveCount(1);
 });
 
-test("should fetch wallets successfully", async ({ context, page, metamaskPage, extensionId }) => {
-  await page.goto("/wallets");
-  const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword, extensionId);
-  await page.getByRole("button", { name: "Connect a wallet" }).click();
-  await page.getByRole("button", { name: "Metamask" }).click();
+test.describe
+  .serial("Adding and deleting wallets", () => {
+    test("should add a wallet successfully", async ({
+      context,
+      page,
+      metamaskPage,
+      extensionId,
+    }) => {
+      await page.goto("/wallets");
+      const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword, extensionId);
+      await page.getByRole("button", { name: "Connect a wallet" }).click();
+      await page.getByRole("button", { name: "Metamask" }).click();
 
-  await metamask.connectToDapp();
-  await page.waitForTimeout(3000);
-  await metamask.confirmSignature();
-  const list = page.locator("#wallets-list");
-  await expect(list.getByRole("listitem")).toHaveCount(1);
-  const address = await metamask.getAccountAddress();
-  const actualAddress = await list.getByRole("listitem").first().locator("p").last().innerText();
-  await expect(normalizeAddress(actualAddress)).toEqual(normalizeAddress(address));
-});
+      await metamask.connectToDapp();
+      await page.waitForTimeout(3000);
+      await metamask.confirmSignature();
 
-test("should add / delete a wallet successfully", async ({
-  context,
-  page,
-  metamaskPage,
-  extensionId,
-}) => {
-  await page.goto("/wallets");
-  const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword, extensionId);
-  await page.getByRole("button", { name: "Connect a wallet" }).click();
-  await page.getByRole("button", { name: "Metamask" }).click();
+      const walletsList = page.locator("#wallets-list");
+      await expect(walletsList).toBeVisible();
 
-  await metamask.connectToDapp();
-  await page.waitForTimeout(3000);
-  await metamask.confirmSignature();
-  // Testing wallet addition
-  const addWalletButton = page.locator("#add-wallet-button");
-  await addWalletButton.click();
+      const walletsCount = await walletsList.getByRole("listitem").count();
 
-  // Generate a new random wallet
-  const walletAddress = generateSimpleEthAddress();
-  await page.locator("#address").fill(walletAddress);
-  await page.locator("#add-wallet-form-submit").click();
-  await metamask.confirmSignature();
-  await page.waitForTimeout(5000);
-  const list = page.locator("#wallets-list");
-  await expect(list.getByRole("listitem")).toHaveCount(2);
+      // Testing wallet addition
+      const addWalletButton = page.locator("#add-wallet-button");
+      await addWalletButton.click();
 
-  // Testing wallet deletion
-  const deleteButton = await list.locator(`#delete-wallet-${walletAddress}`);
-  await deleteButton.click();
-  await page.locator(`#confirm-delete-wallet-${walletAddress}`).click();
-  await metamask.confirmSignature();
-  await page.waitForTimeout(5000);
-  await expect(list.getByRole("listitem")).toHaveCount(1);
-});
+      // Generate a new random wallet
+      const walletAddress = generateSimpleEthAddress();
+      await page.locator("#address").fill(walletAddress);
+      await page.locator("#add-wallet-form-submit").click();
+      await metamask.confirmSignature();
+      await page.waitForTimeout(5000);
+      const list = page.locator("#wallets-list");
+      await expect(list.getByRole("listitem")).toHaveCount(walletsCount + 1);
+
+      // Testing wallet deletion
+      const deleteButton = await list.locator(`#delete-wallet-${walletAddress}`);
+      await deleteButton.click();
+      await page.locator(`#confirm-delete-wallet-${walletAddress}`).click();
+      await metamask.confirmSignature();
+      await page.waitForTimeout(5000);
+      await expect(list.getByRole("listitem")).toHaveCount(walletsCount);
+    });
+
+    test("clean up added wallets", async ({ context, page, metamaskPage, extensionId }) => {
+      const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword, extensionId);
+      // @todo: should we make this CI variable?
+
+      await page.goto("/wallets");
+
+      await page.getByRole("button", { name: "Connect a wallet" }).click();
+      await page.getByRole("button", { name: "Metamask" }).click();
+
+      await metamask.switchAccount("Pristine");
+      await metamask.connectToDapp();
+
+      await page.waitForTimeout(2000);
+      await metamask.confirmSignature();
+
+      const walletsList = page.locator("#wallets-list");
+      await expect(walletsList).toBeVisible();
+
+      const walletsCount = await walletsList.getByRole("listitem").count();
+
+      for (let i = 0; i < walletsCount; i++) {
+        if ((await walletsList.getByRole("listitem").count()) === 1) break;
+
+        let wallet = await walletsList.getByRole("listitem").first();
+
+        let deleteBtn = wallet.getByRole("button", { name: "Delete" });
+        const isDisabled = await deleteBtn.isDisabled();
+
+        if (isDisabled) {
+          wallet = await walletsList.getByRole("listitem").last();
+          deleteBtn = wallet.getByRole("button", { name: "Delete" });
+        }
+
+        await deleteBtn.click();
+
+        const confirmDeleteBtn = page.getByRole("button", { name: "Delete" }).last();
+
+        await confirmDeleteBtn.click();
+        await page.waitForTimeout(1000);
+        await metamask.confirmSignature();
+
+        // Wait for the modal to disappear
+        const deleteModal = await page.locator('[role="alertdialog"]');
+        await expect(deleteModal).not.toBeVisible({ timeout: 10000 });
+      }
+    });
+  });
