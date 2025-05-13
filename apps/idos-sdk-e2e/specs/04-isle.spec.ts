ded42ad51f6e2f18a79c0a01b0ead54ea7bad213
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2025-05-14 00:20:52 +0300

    chore(e2e): adding CI demo URL env var

--- apps/idos-sdk-e2e/specs/04-isle.spec.ts
+++ apps/idos-sdk-e2e/specs/04-isle.spec.ts
@@ -1,12 +1,13 @@
 import crypto from "node:crypto";
 import { testWithSynpress } from "@synthetixio/synpress";
 import { MetaMask, metaMaskFixtures } from "@synthetixio/synpress/playwright";
+import invariant from "tiny-invariant";
 import basicSetup from "../wallet-setup/basic.setup";
 
 const test = testWithSynpress(metaMaskFixtures(basicSetup));
 const { expect } = test;
 
-const consumerAndIssuerUrl = "https://consumer-and-issuer-demo.playground.idos.network/";
+const consumerAndIssuerUrl = process.env.CONSUMER_AND_ISSUER_BASE_URL;
 const dashboardUrl = "https://dashboard.playground.idos.network";
 
 // Helper function to generate random private key
@@ -22,171 +23,181 @@ test.beforeEach(async ({ context, page, metamask }) => {
   await context.clearCookies();
   await page.evaluate(() => window.localStorage.clear());
 });
+const playgroundTest = process.env.CONSUMER_AND_ISSUER_BASE_URL ? test : test.skip;
 
-test("should create a profile successfully using new wallet", async ({
-  context,
-  page,
-  metamaskPage,
-  extensionId,
-}) => {
-  const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword, extensionId);
-  // generate random private key
-  const privateKey = generatePrivateKey();
-  await metamask.importWalletFromPrivateKey(privateKey);
+playgroundTest(
+  "should create a profile successfully using new wallet",
+  async ({ context, page, metamaskPage, extensionId }) => {
+    invariant(consumerAndIssuerUrl, "CONSUMER_AND_ISSUER_BASE_URL is not set");
 
-  await page.goto(consumerAndIssuerUrl);
+    const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword, extensionId);
+    // generate random private key
+    const privateKey = generatePrivateKey();
+    await metamask.importWalletFromPrivateKey(privateKey);
 
-  await page.evaluate(() => {
-    localStorage.setItem("idOS-is-e2e", "true");
-  });
+    await page.goto(consumerAndIssuerUrl);
 
-  await page.getByRole("button", { name: "Get Started now" }).first().click();
-  await page.getByRole("button", { name: "Metamask" }).click();
+    await page.evaluate(() => {
+      localStorage.setItem("idOS-is-e2e", "true");
+    });
 
-  await metamask.connectToDapp();
+    await page.getByRole("button", { name: "Get Started now" }).first().click();
+    await page.getByRole("button", { name: "Metamask" }).click();
 
-  const isleIframe = page.frameLocator("#idOS-isle > iframe");
+    await metamask.connectToDapp();
 
-  const popupPromise = page.waitForEvent("popup");
+    const isleIframe = page.frameLocator("#idOS-isle > iframe");
 
-  const createProfileButton = isleIframe.getByRole("button", { name: "Create idOS profile" });
-  await expect(createProfileButton).toBeVisible();
-  await createProfileButton.click();
+    const popupPromise = page.waitForEvent("popup");
 
-  const unlockButton = page.frameLocator("#idos-enclave-iframe").locator("#unlock");
-  await unlockButton.waitFor({
-    state: "visible",
-  });
-  await unlockButton.click();
+    const createProfileButton = isleIframe.getByRole("button", { name: "Create idOS profile" });
+    await expect(createProfileButton).toBeVisible();
+    await createProfileButton.click();
 
-  const idOSPopup = await popupPromise;
-  const passwordInput = idOSPopup.locator("#idos-password-input");
-  await passwordInput.fill("qwerty");
-  await idOSPopup.getByRole("button", { name: "Create password" }).click();
+    const unlockButton = page.frameLocator("#idos-enclave-iframe").locator("#unlock");
+    await unlockButton.waitFor({
+      state: "visible",
+    });
+    await unlockButton.click();
 
-  await page.waitForTimeout(2000);
-  await metamask.confirmSignature();
+    const idOSPopup = await popupPromise;
+    const passwordInput = idOSPopup.locator("#idos-password-input");
+    await passwordInput.fill("qwerty");
+    await idOSPopup.getByRole("button", { name: "Create password" }).click();
 
-  await page.waitForTimeout(6000);
-  await metamask.confirmSignature();
+    await page.waitForTimeout(2000);
+    await metamask.confirmSignature();
 
-  await expect(isleIframe.locator(".status-badge").first()).toHaveText("not verified");
-  await isleIframe.getByRole("button", { name: "Verify your identity" }).click();
-  await page.waitForTimeout(2000);
+    await page.waitForTimeout(6000);
+    await metamask.confirmSignature();
 
-  await popupPromise;
+    await expect(isleIframe.locator(".status-badge").first()).toHaveText("not verified");
+    await isleIframe.getByRole("button", { name: "Verify your identity" }).click();
+    await page.waitForTimeout(2000);
 
-  const krakenIframe = await page.frameLocator("iframe#kyc-journey");
-  const continueButton = krakenIframe.getByRole("button", { name: "Continue" });
-  await expect(continueButton).toBeVisible();
-  await continueButton.click();
+    await popupPromise;
 
-  await page.waitForTimeout(2000);
+    const krakenIframe = await page.frameLocator("iframe#kyc-journey");
+    const continueButton = krakenIframe.getByRole("button", { name: "Continue" });
+    await expect(continueButton).toBeVisible();
+    await continueButton.click();
 
-  const sumSubIframe = await krakenIframe.frameLocator("#sumsub-websdk-container > iframe");
-  await expect(sumSubIframe).toBeDefined();
+    await page.waitForTimeout(2000);
 
-  await sumSubIframe.getByText("All countries except USA", { exact: true }).click();
-  await sumSubIframe.getByText("Continue", { exact: true }).click();
+    const sumSubIframe = await krakenIframe.frameLocator("#sumsub-websdk-container > iframe");
+    await expect(sumSubIframe).toBeDefined();
 
-  // Select germany and passport
-  await sumSubIframe.getByRole("button", { name: "Issuing country*" }).click();
-  await sumSubIframe.getByRole("textbox", { name: "Search" }).fill("Germany");
-  await sumSubIframe.getByRole("button", { name: "Germany" }).click();
-  await sumSubIframe.getByText("Passport", { exact: true }).click();
-  await sumSubIframe.getByText("Continue", { exact: true }).click();
+    await sumSubIframe.getByText("All countries except USA", { exact: true }).click();
+    await sumSubIframe.getByText("Continue", { exact: true }).click();
 
-  // Upload passport image
-  const fileInput = await sumSubIframe.locator('input[type="file"]').first();
-  await fileInput.setInputFiles("tests/fixtures/passport.jpg");
-  await sumSubIframe.getByText("Continue", { exact: true }).click();
-  await metamask.confirmSignature();
-  await page.waitForTimeout(3000);
-  await metamask.confirmSignature();
-  await page.waitForTimeout(3000);
-  await expect(isleIframe.locator(".status-badge").first()).toHaveText("verified");
+    // Select germany and passport
+    await sumSubIframe.getByRole("button", { name: "Issuing country*" }).click();
+    await sumSubIframe.getByRole("textbox", { name: "Search" }).fill("Germany");
+    await sumSubIframe.getByRole("button", { name: "Germany" }).click();
+    await sumSubIframe.getByText("Passport", { exact: true }).click();
+    await sumSubIframe.getByText("Continue", { exact: true }).click();
 
-  await isleIframe.locator("button[aria-label='View']").click({ timeout: 10000 });
-  await page.waitForTimeout(3000);
+    // Upload passport image
+    const fileInput = await sumSubIframe.locator('input[type="file"]').first();
+    await fileInput.setInputFiles("tests/fixtures/passport.jpg");
+    await sumSubIframe.getByText("Continue", { exact: true }).click();
+    await metamask.confirmSignature();
+    await page.waitForTimeout(3000);
+    await metamask.confirmSignature();
+    await page.waitForTimeout(3000);
+    await expect(isleIframe.locator(".status-badge").first()).toHaveText("verified");
 
-  await expect(isleIframe.locator("p", { hasText: "basic" })).toBeVisible();
-  isleIframe.locator("a", { hasText: "Permissions" }).click();
+    await isleIframe.locator("button[aria-label='View']").click({ timeout: 10000 });
+    await page.waitForTimeout(3000);
 
-  const claimCardButton = await page.getByRole("button", { name: "Claim your card" });
-  await claimCardButton.click();
+    await expect(isleIframe.locator("p", { hasText: "basic" })).toBeVisible();
+    isleIframe.locator("a", { hasText: "Permissions" }).click();
 
-  await expect(claimCardButton).toBeDisabled();
+    const claimCardButton = await page.getByRole("button", { name: "Claim your card" });
+    await claimCardButton.click();
 
-  await page.waitForTimeout(1000);
-  await metamask.confirmSignature();
+    await expect(claimCardButton).toBeDisabled();
 
-  await page.waitForTimeout(1000);
-  await metamask.confirmSignature();
+    await page.waitForTimeout(1000);
+    await metamask.confirmSignature();
 
-  await expect(claimCardButton).not.toBeVisible();
-  await expect(isleIframe.locator("p", { hasText: "KYC DATA" })).toHaveCount(2);
-});
+    await page.waitForTimeout(1000);
+    await metamask.confirmSignature();
+
+    await expect(claimCardButton).not.toBeVisible();
+    await expect(isleIframe.locator("p", { hasText: "KYC DATA" })).toHaveCount(2);
+  },
+);
 
 test.describe
   .serial("Linking wallet to idOS Dashboard and cleaning up", () => {
-    test("should link existing wallet", async ({ context, page, metamaskPage, extensionId }) => {
-      const metamask = new MetaMask(context, metamaskPage, basicSetup.walletPassword, extensionId);
-      // generate random private key
-      const privateKey = Array.from(crypto.getRandomValues(new Uint8Array(32)))
-        .map((b) => b.toString(16).padStart(2, "0"))
-        .join("");
-      await metamask.importWalletFromPrivateKey(privateKey);
-      const walletAddress = await metamask.getAccountAddress();
+    playgroundTest(
+      "should link existing wallet",
+      async ({ context, page, metamaskPage, extensionId }) => {
+        invariant(consumerAndIssuerUrl, "CONSUMER_AND_ISSUER_BASE_URL is not set");
+        const metamask = new MetaMask(
+          context,
+          metamaskPage,
+          basicSetup.walletPassword,
+          extensionId,
+        );
+        // generate random private key
+        const privateKey = Array.from(crypto.getRandomValues(new Uint8Array(32)))
+          .map((b) => b.toString(16).padStart(2, "0"))
+          .join("");
+        await metamask.importWalletFromPrivateKey(privateKey);
+        const walletAddress = await metamask.getAccountAddress();
 
-      await page.goto(consumerAndIssuerUrl);
-      await page.getByRole("button", { name: "Get Started now" }).first().click();
-      await page.getByRole("button", { name: "Metamask" }).click();
+        await page.goto(consumerAndIssuerUrl);
+        await page.getByRole("button", { name: "Get Started now" }).first().click();
+        await page.getByRole("button", { name: "Metamask" }).click();
 
-      await metamask.connectToDapp();
+        await metamask.connectToDapp();
 
-      await page.waitForTimeout(10000);
+        await page.waitForTimeout(10000);
 
-      const isleIframe = page.frameLocator("#idOS-isle > iframe");
+        const isleIframe = page.frameLocator("#idOS-isle > iframe");
 
-      const createProfileButton = isleIframe.getByRole("button", { name: "Login" });
-      await expect(createProfileButton).toBeVisible();
-      await createProfileButton.click();
-      const linkWalletButton = isleIframe.getByRole("button", {
-        name: "Link wallet to idOS Dashboard",
-      });
-      await expect(linkWalletButton).toBeVisible();
-      await linkWalletButton.click();
-      await page.waitForTimeout(3000);
-      const [, dashboardPage] = await context.pages();
+        const createProfileButton = isleIframe.getByRole("button", { name: "Login" });
+        await expect(createProfileButton).toBeVisible();
+        await createProfileButton.click();
+        const linkWalletButton = isleIframe.getByRole("button", {
+          name: "Link wallet to idOS Dashboard",
+        });
+        await expect(linkWalletButton).toBeVisible();
+        await linkWalletButton.click();
+        await page.waitForTimeout(3000);
+        const [, dashboardPage] = await context.pages();
 
-      await dashboardPage.getByRole("button", { name: "Connect a wallet" }).click();
-      await page.getByRole("button", { name: "Metamask" }).click();
-      await metamask.switchAccount("Pristine");
-      await metamask.connectToDapp();
-      await dashboardPage.waitForTimeout(4000);
-      await metamask.confirmSignature();
+        await dashboardPage.getByRole("button", { name: "Connect a wallet" }).click();
+        await page.getByRole("button", { name: "Metamask" }).click();
+        await metamask.switchAccount("Pristine");
+        await metamask.connectToDapp();
+        await dashboardPage.waitForTimeout(4000);
+        await metamask.confirmSignature();
 
-      expect(await dashboardPage.url()).toContain("wallets?add-wallet");
+        expect(await dashboardPage.url()).toContain("wallets?add-wallet");
 
-      const walletAddressInput = dashboardPage.getByRole("textbox", { name: "Wallet address" });
+        const walletAddressInput = dashboardPage.getByRole("textbox", { name: "Wallet address" });
 
-      await expect(walletAddressInput).toHaveValue(walletAddress);
+        await expect(walletAddressInput).toHaveValue(walletAddress);
 
-      const addWalletButton = dashboardPage.getByRole("button", { name: "Add wallet" });
-      await expect(addWalletButton).toBeVisible();
-      await addWalletButton.click();
-      await page.waitForTimeout(1000);
-      await metamask.confirmSignature();
+        const addWalletButton = dashboardPage.getByRole("button", { name: "Add wallet" });
+        await expect(addWalletButton).toBeVisible();
+        await addWalletButton.click();
+        await page.waitForTimeout(1000);
+        await metamask.confirmSignature();
 
-      const addWalletForm = dashboardPage.getByRole("form", { name: "add-wallet-form" });
-      await addWalletForm.waitFor({ state: "detached" });
+        const addWalletForm = dashboardPage.getByRole("form", { name: "add-wallet-form" });
+        await addWalletForm.waitFor({ state: "detached" });
 
-      await page.waitForTimeout(10000);
-      await metamask.confirmSignature();
+        await page.waitForTimeout(10000);
+        await metamask.confirmSignature();
 
-      const minimizedIsle = isleIframe.locator("#minimized-isle");
-      await expect(minimizedIsle).toBeVisible({ timeout: 10000 });
-      await minimizedIsle.click();
-      await expect(isleIframe.locator(".status-badge").first()).toHaveText("verified");
-    });
+        const minimizedIsle = isleIframe.locator("#minimized-isle");
+        await expect(minimizedIsle).toBeVisible({ timeout: 10000 });
+        await minimizedIsle.click();
+        await expect(isleIframe.locator(".status-badge").first()).toHaveText("verified");
+      },
+    );
   });
