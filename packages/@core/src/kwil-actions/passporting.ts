8af7480710b063aeefd9e9896358d5408407e425
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2025-05-15 21:06:44 +0300

    fix: ts-ignore on passporting kwil-actions

--- packages/@core/src/kwil-actions/passporting.ts
+++ packages/@core/src/kwil-actions/passporting.ts
@@ -2,6 +2,7 @@ import nacl from "tweetnacl";
 import { base64Encode } from "../codecs";
 import type { KwilActionClient } from "../kwil-infra";
 
+// @ts-ignore
 export async function getPassportingPeers(kwilClient: KwilActionClient, params: string[]) {
   return Promise.resolve(
     Array.from({ length: 10 }).map((i) => base64Encode(nacl.sign.keyPair().publicKey)),
