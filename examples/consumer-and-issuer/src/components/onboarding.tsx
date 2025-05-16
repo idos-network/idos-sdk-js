b8ef03ba41bf0d5eca71127cd59c45e03306cb11
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2025-05-17 00:35:32 +0300

    fix(ci-demo): wrapping up acme card migration

--- examples/consumer-and-issuer/src/components/onboarding.tsx
+++ examples/consumer-and-issuer/src/components/onboarding.tsx
@@ -25,7 +25,7 @@ import {
 import { useOnboardingStore } from "@/app/stores/onboarding";
 import { useIsleController } from "@/isle.provider";
 import { KYCJourney } from "./kyc-journey";
-import { MatchingCredential } from "./matching-credential";
+import { MatchingCredential } from "./matching-acme-credential";
 
 function StepIcon({ icon }: { icon: React.ReactNode }) {
   return (
