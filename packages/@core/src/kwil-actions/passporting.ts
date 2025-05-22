f32750233997d67c8f54a0163c3fc49479e0501b
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2025-05-22 14:26:49 +0300

    refactor: remove unneeded imports

--- packages/@core/src/kwil-actions/passporting.ts
+++ packages/@core/src/kwil-actions/passporting.ts
@@ -1,9 +1,6 @@
-import nacl from "tweetnacl";
-import { base64Encode, hexEncode } from "../codecs";
 import type { KwilActionClient } from "../kwil-infra";
 import type { PassportingPeer } from "../types";
 
-// @ts-ignore
 export async function getPassportingPeers(
   kwilClient: KwilActionClient,
 ): Promise<PassportingPeer[]> {
