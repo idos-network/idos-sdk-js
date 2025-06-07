4149b113218c547939405e17ff9410f6c973e2a8
Jan Strnádek <jan.strnadek@gmail.com>
2025-06-07 13:31:57 +0200

    feat: Add vercel preset in react-router

--- examples/foxy-demo/react-router.config.ts
+++ examples/foxy-demo/react-router.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from "@vercel/react-router/vite";
 
 declare module "react-router" {
   interface Future {
@@ -13,4 +14,5 @@ export default {
   future: {
     unstable_middleware: true,
   },
+  presets: [vercelPreset()],
 } satisfies Config;
