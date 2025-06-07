42f57ba0dd8e9e434f88c0555bb0eca2beab4c02
Jan Strnádek <jan.strnadek@gmail.com>
2025-06-07 11:47:12 +0200

    build: Fix vercels build

--- examples/foxy-demo/vite.config.ts
+++ examples/foxy-demo/vite.config.ts
@@ -6,14 +6,15 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(({ isSsrBuild }) => ({
   build: {
     target: isSsrBuild ? "node22" : "esnext",
-  },
-  rollupOptions: {
-    external: [
-      "@digitalbazaar/ed25519-signature-2020",
-      "@digitalbazaar/ed25519-verification-key-2020",
-      "@digitalbazaar/vc",
-      "base85",
-    ],
+    rollupOptions: {
+      external: [
+        "@digitalbazaar/ed25519-signature-2020",
+        "@digitalbazaar/ed25519-verification-key-2020",
+        "@digitalbazaar/vc",
+        "jsonld-document-loader",
+        "base85",
+      ],
+    },
   },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 }));
