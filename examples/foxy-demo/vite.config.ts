e402a60024ded930b153bcd53084a902a47d575b
Jan Strnádek <jan.strnadek@gmail.com>
2025-06-07 13:26:05 +0200

    Fix build

--- examples/foxy-demo/vite.config.ts
+++ examples/foxy-demo/vite.config.ts
@@ -6,15 +6,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig(({ isSsrBuild }) => ({
   build: {
     target: isSsrBuild ? "node22" : "esnext",
-    rollupOptions: {
-      external: [
-        "@digitalbazaar/ed25519-signature-2020",
-        "@digitalbazaar/ed25519-verification-key-2020",
-        "@digitalbazaar/vc",
-        "jsonld-document-loader",
-        "base85",
-      ],
-    },
   },
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 }));
