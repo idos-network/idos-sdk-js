28f0787d12a9840ad9d6a962131d61d933db68d6
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2025-07-07 10:50:42 +0300

    chore: bootstrap neobank app

---
+++ examples/neobank/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="grid h-screen place-content-center">
+      <h1 className="font-semibold text-2xl">NeoBank</h1>
+    </div>
+  );
+}
