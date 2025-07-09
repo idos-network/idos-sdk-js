5f5b7ed5392f66607f41b34105a209282f5420c4
Alexandr Kovalenko <alex.sapience@gmail.com>
2025-07-09 18:59:43 +0300

    Change http to https

--- apps/idos-enclave/src/lib/mpc/engine-client.ts
+++ apps/idos-enclave/src/lib/mpc/engine-client.ts
@@ -16,7 +16,10 @@ export class EngineClient {
 
   constructor(baseUrl: string, contractAddress: PbcAddress) {
     this.contractAddress = contractAddress;
-    this.baseUrl = baseUrl;
+    // Force https for baseUrl
+    this.baseUrl = baseUrl.replace(/^http:/, "https:");
+    // TODO: get back pure baseUrl when new contract deployment will be fixed
+    // this.baseUrl = baseUrl;
   }
 
   public async sendUpload(id: string, uploadRequest: Sharing, signature: Bytes): Promise<string> {
