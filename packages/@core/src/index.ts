775b638070cee1580d49dda35797da05c8f237e9
Jan Strnádek <jan.strnadek@gmail.com>
2025-07-15 15:13:18 +0200

    fix(utils): Remove store export from core

--- packages/@core/src/index.ts
+++ packages/@core/src/index.ts
@@ -2,6 +2,5 @@ export * from "./codecs";
 export * from "./cryptography";
 export * from "./kwil-actions";
 export * from "./kwil-infra";
-export * from "./store";
 export * from "./types";
 export * from "./utils";
