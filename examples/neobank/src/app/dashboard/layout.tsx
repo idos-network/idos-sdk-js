eea4425e8acfc7b640dff1710917eed311e54756
ditoglez <ditoglez@gmail.com>
2025-07-15 19:07:29 +0300

    feat(neobank): fix small layout issues (#973)

--- examples/neobank/src/app/dashboard/layout.tsx
+++ examples/neobank/src/app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen flex-col px-5 py-12">
       <div className="mx-auto flex w-full max-w-[1022px] flex-col gap-12">
-        <div className="flex flex-col justify-between gap-12 md:flex-row md:items-center">
+        <div className="flex flex-col justify-between gap-12 lg:flex-row lg:items-center">
           <BalanceComponent />
           <ActionToolbar />
         </div>
