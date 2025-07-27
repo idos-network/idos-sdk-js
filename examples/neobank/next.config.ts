8edf6bcaad6bcc8d3e1770eab21d08b827c57727
Mohammed-Mamoun98 <modahmada2018@gmail.com>
2025-07-27 22:38:46 +0300

    fix(neobank): remove webpackMemoryOpt flag

--- examples/neobank/next.config.ts
+++ examples/neobank/next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    webpackMemoryOptimizations: true,
-  },
 };
 
 export default nextConfig;
