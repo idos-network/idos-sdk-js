aa3aecc5bc45e4ff58c68e6942ea18febcf8a40f
Jan Strnádek <jan.strnadek@gmail.com>
2025-07-29 14:41:46 +0200

    fix(enclave): Method names

--- packages/client/src/enclave/iframe-enclave.ts
+++ packages/client/src/enclave/iframe-enclave.ts
@@ -111,16 +111,12 @@ export class IframeEnclave extends BaseProvider<IframeEnclaveOptions> {
   }
 
   /** @override parent method to call iframe */
-<<<<<<< HEAD
   async ensureUserEncryptionProfile(): Promise<PublicEncryptionProfile> {
-=======
-  async getPublicEncryptionProfile(): Promise<PublicEncryptionProfile> {
->>>>>>> f03b22fe (fix(enclave#backup): Fix enclave backup passwords)
     this.showEnclave();
 
     try {
       return await this.requestToEnclave({
-        getPublicEncryptionProfile: {},
+        ensureUserEncryptionProfile: {},
       });
     } finally {
       this.hideEnclave();
