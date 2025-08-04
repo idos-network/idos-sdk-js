934d8e84445f4aca3c004195f791be9006824705
ditoglez <ditoglez@gmail.com>
2025-08-04 10:19:16 +0300

    feat(core): validate complex params with zod onm kwil actions (#1009)

--- packages/@core/src/kwil-actions/wallets.ts
+++ packages/@core/src/kwil-actions/wallets.ts
@@ -1,15 +1,29 @@
+import z from "zod";
 import type { KwilActionClient } from "../kwil-infra";
 import type { idOSWallet } from "../types";
 
-export interface UpsertWalletParams {
-  id: string;
-  user_id: string;
-  address: string;
-  wallet_type: string;
-  message: string;
-  public_key: string;
-  signature: string;
-}
+const UpsertWalletParamsSchema: z.ZodObject<
+  {
+    id: z.ZodString;
+    user_id: z.ZodString;
+    address: z.ZodString;
+    wallet_type: z.ZodString;
+    message: z.ZodString;
+    public_key: z.ZodString;
+    signature: z.ZodString;
+  },
+  "strip"
+> = z.object({
+  id: z.string(),
+  user_id: z.string(),
+  address: z.string(),
+  wallet_type: z.string(),
+  message: z.string(),
+  public_key: z.string(),
+  signature: z.string(),
+});
+
+export type UpsertWalletParams = z.infer<typeof UpsertWalletParamsSchema>;
 
 /**
  * Upserts a wallet as an `inserter`.
@@ -18,31 +32,48 @@ export async function upsertWalletAsInserter(
   kwilClient: KwilActionClient,
   params: UpsertWalletParams,
 ): Promise<UpsertWalletParams> {
+  const input = UpsertWalletParamsSchema.parse(params);
   await kwilClient.execute({
     name: "upsert_wallet_as_inserter",
     description: "Add a wallet to idOS",
-    inputs: params,
+    inputs: input,
   });
 
   return params;
 }
 
-export interface AddWalletParams {
-  id: string;
-  address: string;
-  public_key: string | null;
-  message: string;
-  signature: string;
-}
+const AddWalletParamsSchema: z.ZodObject<
+  {
+    id: z.ZodString;
+    address: z.ZodString;
+    public_key: z.ZodString;
+    message: z.ZodString;
+    signature: z.ZodString;
+    user_id: z.ZodString;
+    wallet_type: z.ZodString;
+  },
+  "strip"
+> = z.object({
+  id: z.string(),
+  address: z.string(),
+  public_key: z.string(),
+  message: z.string(),
+  signature: z.string(),
+  user_id: z.string(),
+  wallet_type: z.string(),
+});
+
+export type AddWalletParams = z.infer<typeof AddWalletParamsSchema>;
 
 export async function addWallet(
   kwilClient: KwilActionClient,
   params: AddWalletParams,
 ): Promise<AddWalletParams> {
+  const input = AddWalletParamsSchema.parse(params);
   await kwilClient.execute({
     name: "add_wallet",
     description: "Add a wallet to idOS",
-    inputs: params,
+    inputs: input,
   });
 
   return params;
