836ed14a063e865c6458cce51224c0e969b1416b
Jan Strnádek <jan.strnadek@gmail.com>
2025-08-25 16:20:51 +0200

    feat(kraken): Force wallet address

--- examples/pay-demo/app/providers/kraken.server.ts
+++ examples/pay-demo/app/providers/kraken.server.ts
@@ -27,7 +27,7 @@ export const fetchSharedToken = async (credentialId: string) => {
   return data.token;
 };
 
-export const generateKrakenUrl = async (type: string) => {
+export const generateKrakenUrl = async (type: string, walletAddress: string) => {
   const payload = {
     clientId: SERVER_ENV.KRAKEN_CLIENT_ID,
     kyc: true,
@@ -37,7 +37,7 @@ export const generateKrakenUrl = async (type: string) => {
 
   const token = jwt.sign(payload, SERVER_ENV.KRAKEN_PRIVATE_KEY, { algorithm: "ES512" });
 
-  return `${SERVER_ENV.KRAKEN_API_URL}/kyc?token=${token}&provider=${type}`;
+  return `${SERVER_ENV.KRAKEN_API_URL}/kyc?token=${token}&provider=${type}&walletAddress=${encodeURIComponent(walletAddress)}`;
 };
 
 export async function getKrakenToken(): Promise<string> {
