de99a27a49530030f8180dbd32b6c5cf03c65b85
Jan Strnádek <jan.strnadek@gmail.com>
2025-09-23 09:05:27 +0200

    fix(build): Fix builds

--- examples/pay-demo/app/providers/idos.server.ts
+++ examples/pay-demo/app/providers/idos.server.ts
@@ -1,4 +1,4 @@
-import { type Credentials, idOSConsumer as idOSConsumerClass } from "@idos-network/consumer";
+import { type Credential, idOSConsumer as idOSConsumerClass } from "@idos-network/consumer";
 import nacl from "tweetnacl";
 import { COMMON_ENV } from "./envFlags.common";
 import { SERVER_ENV } from "./envFlags.server";
@@ -26,7 +26,7 @@ export async function getSharedCredential(credentialId: string, inserterId?: str
   const credentialContents: string =
     await idOSConsumer.getSharedCredentialContentDecrypted(credentialId);
 
-  const data = JSON.parse(credentialContents) as Credentials;
+  const data = JSON.parse(credentialContents) as Credential;
 
   // Verify the credential
   const [verificationResult] = await idOSConsumer.verifyCredential(data, [
