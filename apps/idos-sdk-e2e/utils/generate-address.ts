0db1a50894daff3b7b4c3c9fe4a72b7dd746aa3a
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2025-11-10 21:33:16 +0200

    chore: drop e2e app

--- apps/idos-sdk-e2e/utils/generate-address.ts
+++
@@ -1,9 +0,0 @@
-export function generateSimpleEthAddress() {
-  // Generate 40 random hex characters (20 bytes)
-  const chars = "0123456789abcdef";
-  let address = "";
-  for (let i = 0; i < 40; i++) {
-    address += chars[Math.floor(Math.random() * chars.length)];
-  }
-  return `0x${address}`;
-}
