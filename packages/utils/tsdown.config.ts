d4446d6786b7c298872e05fbaff9cff6895458b0
Jan Strnádek <jan.strnadek@gmail.com>
2025-11-24 10:19:15 +0100

    Fix dependencies

--- packages/utils/tsdown.config.ts
+++ packages/utils/tsdown.config.ts
@@ -11,4 +11,11 @@ export default defineConfig({
     "./src/xrpl-credentials/index.ts",
   ],
   dts: true,
+  external: [
+    "@digitalbazaar/ed25519-signature-2020",
+    "@digitalbazaar/ed25519-verification-key-2020",
+    "@digitalbazaar/vc",
+    "base85",
+    "jsonld-document-loader",
+  ],
 });
