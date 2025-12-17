5162a4aa87b6afb58b90d47224c9d373db396e2c
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2025-12-17 23:38:04 +0200

    feat(wallet-connector): update layout

--- apps/wallet-connector/src/app.tsx
+++ apps/wallet-connector/src/app.tsx
@@ -2,17 +2,58 @@ import { EVMConnector } from "@/components/evm-connector";
 import { NearConnector } from "@/components/near-connector";
 import { StellarConnector } from "@/components/stellar-connector";
 import { XRPLConnector } from "@/components/xrpl-connector";
+import { useStore } from "@/state";
+
+function Center({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-full items-center justify-center gap-6 p-6 max-w-sm mx-auto">
+      <div className="flex flex-col gap-4 items-stretch w-full">{children}</div>
+    </div>
+  );
+}
 
 function App() {
-  return (
-    <div className="grid h-full place-content-center gap-6 p-6">
-      <div className="flex flex-col gap-4">
+  const connectedWallet = useStore((state) => state.connectedWallet);
+
+  if (connectedWallet === "evm") {
+    return (
+      <Center>
         <EVMConnector />
+      </Center>
+    );
+  }
+
+  if (connectedWallet === "near") {
+    return (
+      <Center>
         <NearConnector />
+      </Center>
+    );
+  }
+
+  if (connectedWallet === "stellar") {
+    return (
+      <Center>
         <StellarConnector />
+      </Center>
+    );
+  }
+
+  if (connectedWallet === "xrpl") {
+    return (
+      <Center>
         <XRPLConnector />
-      </div>
-    </div>
+      </Center>
+    );
+  }
+
+  return (
+    <Center>
+      <EVMConnector />
+      <NearConnector />
+      <StellarConnector />
+      <XRPLConnector />
+    </Center>
   );
 }
 
