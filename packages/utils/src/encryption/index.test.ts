b1bd61478f560397f0d1dc8c060add0fc4a49bd6
Jan Strnádek <jan@idos.network>
2026-02-13 06:56:28 +0100

    Upgrade from old scrypt-js to noble (#1334)

--- packages/utils/src/encryption/index.test.ts
+++ packages/utils/src/encryption/index.test.ts
@@ -6,6 +6,16 @@ import { decrypt, encrypt, keyDerivation } from "./index.js";
 describe("encryption", () => {
   const helloMessage = new Uint8Array([104, 101, 108, 108, 111]);
 
+  it("check key derivation from scrypt-js", async () => {
+    const password = "correct horse battery staple";
+    const salt = "9f51b3b2-4cbe-4c2b-8ea3-0b0c1b2f1a11";
+    const expectedKey = "8iTwhZxrtQGKVV5OMthD5DxFc8c6B8h9MnU2Sh6JTTM=";
+
+    const first = await keyDerivation(password, salt);
+
+    expect(base64Encode(first)).toBe(expectedKey);
+  });
+
   it("keyDerivation is deterministic for same inputs", async () => {
     const password = "correct horse battery staple";
     const salt = "9f51b3b2-4cbe-4c2b-8ea3-0b0c1b2f1a11";
