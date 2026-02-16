9bd7e97e5f344554138cbfa4614ffeef93e5eb55
Jan Strnádek <jan.strnadek@gmail.com>
2026-02-16 08:49:38 +0100

    Fix pipeline

--- apps/facesign-enclave/src/app.tsx
+++ apps/facesign-enclave/src/app.tsx
@@ -1,16 +1,10 @@
 import { Outlet } from "react-router";
 import idosLogo from "./assets/idos-logo.svg";
-import { KeyStorageContextProvider } from "./contexts/key";
-import { RequestsContextProvider } from "./contexts/requests";
 
 export function App() {
   return (
     <div className="w-full h-full bg-black relative">
-      <KeyStorageContextProvider>
-        <RequestsContextProvider>
-          <Outlet />
-        </RequestsContextProvider>
-      </KeyStorageContextProvider>
+      <Outlet />
       <div className="fixed bottom-4 right-4 flex items-center gap-2 text-white opacity-70">
         <span className="text-sm">Powered by</span>
         <img src={idosLogo} alt="idOS" className="h-6" />
