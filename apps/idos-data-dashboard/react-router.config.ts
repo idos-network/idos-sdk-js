10fe936f18fbe00cb74abaa2bb1b9b551c5509e0
Jan Strnádek <jan.strnadek@gmail.com>
2026-03-03 13:10:30 +0100

    Try to fix more (remove source map upload)

--- apps/idos-data-dashboard/react-router.config.ts
+++ apps/idos-data-dashboard/react-router.config.ts
@@ -1,5 +1,4 @@
 import type { Config } from "@react-router/dev/config";
-import { sentryOnBuildEnd } from "@sentry/react-router";
 import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
@@ -14,12 +13,4 @@ export default {
   },
 
   presets: [vercelPreset()],
-
-  buildEnd: async ({ viteConfig, reactRouterConfig, buildManifest }) => {
-    await sentryOnBuildEnd({
-      viteConfig: viteConfig,
-      reactRouterConfig: reactRouterConfig,
-      buildManifest: buildManifest,
-    });
-  },
 } satisfies Config;
--- apps/idos-data-dashboard/vite.config.ts
+++ apps/idos-data-dashboard/vite.config.ts
@@ -1,6 +1,5 @@
 import path from "node:path";
 import { reactRouter } from "@react-router/dev/vite";
-import { type SentryReactRouterBuildOptions, sentryReactRouter } from "@sentry/react-router";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
@@ -12,33 +11,14 @@ if (process.env.VERCEL_ENV && !process.env.VITE_SENTRY_ENV) {
   process.env.VITE_SENTRY_ENV = `${process.env.VERCEL_ENV}@${process.env.VERCEL_DEPLOYMENT_ID}`;
 }
 
-const sentryConfig: SentryReactRouterBuildOptions = {
-  org: "idos-network",
-  project: "data-dashboard",
-  // An auth token is required for uploading source maps;
-  // store it in an environment variable to keep it secure.
-  authToken: process.env.SENTRY_AUTH_TOKEN,
-  sourceMapsUploadOptions: {
-    enabled: ["production", "playground"].includes(process.env.VERCEL_ENV ?? ""),
-  },
-};
-
 // https://vitejs.dev/config/
 export default defineConfig(async (config) => {
-  const plugins = [
-    tailwindcss(),
-    reactRouter(),
-    tsconfigPaths(),
-    mkcert(),
-    // https://github.com/getsentry/sentry-javascript/blob/master/dev-packages/e2e-tests/test-applications/react-router-7-framework-instrumentation/vite.config.ts#L9C77-L9C86
-    // biome-ignore lint/suspicious/noExplicitAny: Expected
-    ...((await sentryReactRouter(sentryConfig, config)) as any[]),
-  ];
+  const plugins = [tailwindcss(), reactRouter(), tsconfigPaths(), mkcert()];
 
   if (config.isSsrBuild) {
     plugins.push(
       nodePolyfills({
-        exclude: ["stream"],
+        exclude: ["stream", "crypto"],
         globals: {
           Buffer: true,
         },
