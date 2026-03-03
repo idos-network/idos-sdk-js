18a85c6933baafa8fbcae0ec1014ebb89eb144d0
Jan Strnádek <jan.strnadek@gmail.com>
2026-03-03 13:20:31 +0100

    Try to fix build

--- apps/idos-data-dashboard/react-router.config.ts
+++ apps/idos-data-dashboard/react-router.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "@react-router/dev/config";
+import { sentryOnBuildEnd } from "@sentry/react-router";
 import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
@@ -13,4 +14,12 @@ export default {
   },
 
   presets: [vercelPreset()],
+
+  buildEnd: async ({ viteConfig, reactRouterConfig, buildManifest }) => {
+    await sentryOnBuildEnd({
+      viteConfig: viteConfig,
+      reactRouterConfig: reactRouterConfig,
+      buildManifest: buildManifest,
+    });
+  },
 } satisfies Config;
