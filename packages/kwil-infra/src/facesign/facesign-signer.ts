1935250eb5267f2a4860a7330ad6c0bce736be65
ditoglez <fernando@idos.network>
2026-03-04 13:03:38 +0200

    fix: facesign enclave dialog centering (#1381)

--- packages/kwil-infra/src/facesign/facesign-signer.ts
+++ packages/kwil-infra/src/facesign/facesign-signer.ts
@@ -254,10 +254,14 @@ export class FaceSignSignerProvider {
     const container = document.createElement("dialog");
     Object.assign(container.style, {
       position: "fixed",
-      top: "0",
-      left: "0",
+      inset: "0",
       width: "100%",
       height: "100%",
+      maxWidth: "100%",
+      maxHeight: "100%",
+      margin: "0",
+      padding: "0",
+      border: "none",
       zIndex: "99999",
       background: "#0a0a0a",
     });
