c680863653676a5c5be32980b737ad6f6f40d65a
Jan Strnádek <jan.strnadek@gmail.com>
2026-04-11 08:04:17 +0200

    Move to vite plugin react

--- apps/embedded-wallet/vite.config.ts
+++ apps/embedded-wallet/vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
