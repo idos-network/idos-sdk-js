c5d3701c0c2dc715d955b751d9d7269de082e55c
Jan Strnádek <jan.strnadek@gmail.com>
2026-04-13 10:33:44 +0200

    Fix QR code in facesign enclave

--- apps/facesign-enclave/vite.config.ts
+++ apps/facesign-enclave/vite.config.ts
@@ -1,14 +1,10 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { resolve } from "node:path";
 import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
   resolve: {
-    alias: {
-      "@": resolve(__dirname, "./app"),
-    },
     tsconfigPaths: true,
   },
   server: {
